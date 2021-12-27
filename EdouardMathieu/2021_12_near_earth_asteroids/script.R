dd3a7d721530c9c6a038d175f21b8f0c92d3e149
Edouard Mathieu <edmat@pm.me>
2021-12-27 14:23:05 +0100

    Update script.R

--- EdouardMathieu/2021_12_near_earth_asteroids/script.R
+++ EdouardMathieu/2021_12_near_earth_asteroids/script.R
@@ -1,15 +1,20 @@
 library(stringr)
 library(data.table)
 library(lubridate)
-rm(list = ls())
 
+# Source URL: https://cneos.jpl.nasa.gov/stats/totals.html
+# Tab: Cumulative Totals
+# Scroll down and click "CSV" under the table
 df <- fread("Discovery StatisticsPrintDownload.csv", select = c("Date", "NEA-km", "NEA-140m", "NEA"))
+setorder(df, -Date)
 df[, Year := str_sub(Date, 1, 4)]
+df <- df[, .SD[1], Year]
+
 df[, larger_than_1km := `NEA-km`]
 df[, between_140m_and_1km := `NEA-140m` - larger_than_1km]
 df[, smaller_than_140m := NEA - larger_than_1km - between_140m_and_1km]
-setorder(df, -Date)
+
 df[, Entity := "World"]
-df <- df[, .SD[1], Year][, c("Entity", "Year", "larger_than_1km", "between_140m_and_1km", "smaller_than_140m")]
+df <- df[, c("Entity", "Year", "larger_than_1km", "between_140m_and_1km", "smaller_than_140m")]
 
 fwrite(df, "Near-Earth asteroids discovered over time.csv")
