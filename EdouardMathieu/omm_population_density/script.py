4c78bc2e9313939e372034d0b3e14c2fdadd4b4f
Edouard Mathieu <edmat@pm.me>
2022-02-16 19:47:42 +0100

    enhance(pop-density): use owid module
    xref https://github.com/owid/owid-issues/issues/120

--- EdouardMathieu/omm_population_density/script.py
+++ EdouardMathieu/omm_population_density/script.py
@@ -3,37 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-def get_var_from_chart(chart_slug: str, var_name: str) -> pd.DataFrame:
-    url = f"https://ourworldindata.org/grapher/{chart_slug}"
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
-    for link in soup.find_all("link"):
-        if link.get("rel") == ["preload"]:
-            json_url = f"https://ourworldindata.org/{link.get('href')}"
-    response = requests.get(json_url).json()
-
-    data = list(response["variables"].values())[0]
-    data = pd.DataFrame(
-        {
-            "year": data["years"],
-            "entity_id": data["entities"],
-            var_name: data["values"],
-        }
-    )
-
-    entities = (
-        pd.DataFrame.from_dict(response["entityKey"], orient="index")
-        .reset_index()
-        .rename(columns={"index": "entity_id"})
-    )
-    entities["entity_id"] = entities["entity_id"].astype(int)
-
-    df = (
-        pd.merge(data, entities, on="entity_id", validate="many_to_one")
-        .drop(columns=["entity_id", "code"])
-        .rename(columns={"name": "entity"})
-    )
-    return df
+from owid import site
 
 
 def main():
@@ -41,16 +11,19 @@ def main():
     # Land area is fetched dynamically from the variable currently used an the input of our main
     # land area chart: https://ourworldindata.org/grapher/land-area-km
     land_area = (
-        get_var_from_chart(chart_slug="land-area-km", var_name="land_area")
+        site.get_chart_data(slug="land-area-km")
+        .rename(columns={"value": "land_area"})
         .groupby("entity", as_index=False)
         .head(1)
-        .drop(columns="year")
+        .drop(columns=["year", "variable"])
     )
 
     # Population is fetched dynamically from the variable currently used an the input of our main
     # population chart: https://ourworldindata.org/grapher/population-past-future
-    population = get_var_from_chart(
-        chart_slug="population-past-future", var_name="population"
+    population = (
+        site.get_chart_data(slug="population-past-future")
+        .rename(columns={"value": "population"})
+        .drop(columns=["variable"])
     )
 
     df = pd.merge(land_area, population, on="entity", validate="one_to_many")
