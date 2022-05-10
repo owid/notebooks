a3cabd8ac5d92df3285745ea98b1011743a292f7
Edouard Mathieu <edmat@pm.me>
2022-05-10 10:32:17 +0200

    get age group data

--- EdouardMathieu/yougov_dietary_choices/script.R
+++ EdouardMathieu/yougov_dietary_choices/script.R
@@ -3,19 +3,21 @@ library(stringr)
 library(data.table)
 library(lubridate)
 
-url <- "https://yougov.co.uk/_pubapis/v5/uk/trackers/dietery-choices-of-brits-eg-vegeterian-flexitarian-meat-eater-etc/download/"
+URL <- "https://yougov.co.uk/_pubapis/v5/uk/trackers/dietery-choices-of-brits-eg-vegeterian-flexitarian-meat-eater-etc/download/"
 
-df <- rio::import(url, format = "xlsx")
-setDT(df)
+get_sheet_data <- function(age_group) {
+  df <- rio::import(URL, format = "xlsx", sheet = "All adults")
+  setDT(df)
+  setnames(df, "Which of these best describes your diet?", "Entity")
+  df <- df[!Entity %in% c("Base", "Unweighted base")]
+  df[, Entity := str_replace(Entity, " \\(.*", "")]
+  df <- transpose(df, make.names = TRUE, keep.names = "Year")
+  df[, Entity := age_group]
+  return(df)
+}
 
-setnames(df, "Which of these best describes your diet?", "Entity")
-df <- df[!Entity %in% c("Base", "Unweighted base")]
-df[, Entity := str_replace(Entity, " \\(.*", "")]
 
-df <- transpose(df, make.names = TRUE, keep.names = "Year")
-df[, Entity := "United Kingdom"]
 
 setcolorder(df, c("Entity", "Year"))
-
 df[, Year := as.integer(ymd(Year) - ymd("20190101"))]
 fwrite(df, "YouGov - Dietary choices of Brits.csv")
