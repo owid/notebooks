4186c6f51a74311aa3ac26b13ee19300680b7802
Edouard Mathieu <edmat@pm.me>
2022-05-25 08:51:16 +0200

    twiter update

--- EdouardMathieu/monkeypox/monkeypox.R
+++ EdouardMathieu/monkeypox/monkeypox.R
@@ -75,3 +75,16 @@ df[, date := date(date)]
 df <- df[date < today()]
 setorder(df, location, date)
 fwrite(df, "owid-monkeypox-data.csv")
+
+# Twitter update
+max <- df[location != "World" & !is.na(total_confirmed_by_confirmation), max(total_confirmed_by_confirmation, na.rm = T), location]
+setorder(max, -V1)
+update <- sprintf(
+  "Monkeypox update:\n%s total confirmed cases\n+%s per day in the last week\n\nCountries with the most confirmed cases:\n%s\n\nData by @globaldothealth",
+  max(df$total_confirmed_by_confirmation, na.rm = T),
+  round(tail(df$`7day_confirmed_by_confirmation`, 1)),
+  paste0(sprintf("%s: %s", head(max$location, 5), head(max$V1, 5)), collapse = "\n")
+)
+clipr::write_clip(update)
+browseURL("https://ourworldindata.org/explorers/monkeypox?facet=none&hideControls=false&Metric=Confirmed+cases&Frequency=7-day+average&Shown+by=Date+of+confirmation&country=~OWID_WRL")
+browseURL("https://twitter.com/compose/tweet")
