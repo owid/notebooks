88dd72b132bda768f35e1721717acdbe692956cc
Edouard Mathieu <edmat@pm.me>
2022-05-27 12:29:29 +0200

    Update tweet.R

--- EdouardMathieu/monkeypox/tweet.R
+++ EdouardMathieu/monkeypox/tweet.R
@@ -3,12 +3,14 @@ explorer_url <- "https://ourworldindata.org/explorers/monkeypox?facet=none&hideC
 top <- df[location != "World" & !is.na(total_confirmed_by_confirmation), max(total_confirmed_by_confirmation, na.rm = T), location]
 setorder(top, -V1)
 update <- sprintf(
-  "Monkeypox update:\n%s total confirmed cases\n+%s per day (7-day average)\n\nCountries with the most confirmed cases:\n%s\n\nData by @globaldothealth: %s",
+  "Monkeypox update:\n%s total confirmed cases in %s countries\n+%s per day (7-day average)\n\nCountries with the most confirmed cases:\n%s\n\nData by @globaldothealth: %s",
   max(df$total_confirmed_by_confirmation, na.rm = T),
+  df[total_confirmed_by_confirmation > 0, length(unique(location))],
   round(latest_avg),
   paste0(sprintf("%s: %s", head(top$location, 5), head(top$V1, 5)), collapse = "\n"),
   explorer_url
 )
+
 clipr::write_clip(update)
 browseURL(explorer_url)
 browseURL("https://twitter.com/compose/tweet")
