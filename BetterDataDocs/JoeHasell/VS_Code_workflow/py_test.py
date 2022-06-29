c66c921815808ab72fbb53ed5944f67e1291dc73
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-06-29 11:19:23 +0100

    Adding markdown library

--- BetterDataDocs/JoeHasell/VS_Code_workflow/py_test.py
+++ BetterDataDocs/JoeHasell/VS_Code_workflow/py_test.py
@@ -1,4 +1,10 @@
+# %%
 
+
+from IPython.display import Markdown as md
+
+# %%
+variableName = "This header is stored as a Variable"
 
 # %%
 print("This is a print out from code")
@@ -7,8 +13,13 @@ print("This is a print out from code")
 # This is markdown:
 # * So is this
 # * And this
-
+#
 # # And this.
+
+# %%
+# You can also use variables in markdown inline using the Markdown library, like this:
+md("## {}".format(variableName))
+
 
 
 
