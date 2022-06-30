73abdcc8b63e8eb5423f2c6a0075b0c66cf670de
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-06-30 11:34:25 +0100

    Add variable metadata format

--- BetterDataDocs/PabloArriagada/pwt/pwt_pipeline.py
+++ BetterDataDocs/PabloArriagada/pwt/pwt_pipeline.py
@@ -1,4 +1,5 @@
 from IPython.display import Markdown as md
+from IPython.core.display import display, HTML
 
 
 # %% [markdown]
@@ -6,40 +7,57 @@ from IPython.display import Markdown as md
 
 
 # %% [markdown]
-
-# *This article describes the data in the Penn World Tables version 10.0 and documents how Our World in Data have handled and transformed this data in order to make use of it in our publication.*
+"""
+*This article describes the data in the Penn World Tables version 10.0 
+and documents how Our World in Data have handled and transformed this data 
+in order to make use of it in our publication.*
 #
-# *This article is an unsual, experimental format, which we have designed to make our data work more transparent and reusable.*
+*This article is an unsual, experimental format, which we have designed 
+to make our data work more transparent and reusable.*
 #
-# *To prepare the data for use in our publication we write and then execute a computer programme. Within that computer programme we include extensive notes, explanations and visualizations to make any choices concerning the treatment of the data more visible and to explain our reasoning. This article is a web version of that computer programme in which priority is given to the notes and explanations and much of the code is hidden or not shown to improve readability. You can read this article whether or not you are familiar with (python) code in order to understand more about the Penn World Tables and our treatment of the data.*
+*To prepare the data for use in our publication we write and then execute
+ a computer programme. Within that computer programme we include extensive 
+ notes, explanations and visualizations to make any choices concerning the 
+ treatment of the data more visible and to explain our reasoning. 
+ This article is a web version of that computer programme in which 
+ priority is given to the notes and explanations and much of the code 
+ is hidden or not shown to improve readability. You can read this 
+ article whether or not you are familiar with (python) code in order 
+ to understand more about the Penn World Tables and our treatment of the data.*
 #
-# *The full code we use to prepare this data can be found in GitHub.*
-
+*The full code we use to prepare this data can be found in here GitHub. (provide link)*
+"""
 
 # ## About this data
 
 # %%
-# Provide dataset metadata (as specified in Grapher Admin)
-datasetName = "Penn World Tables version 10.0"
-datasetSourceName = "Penn World Tables"
-datasetLink = "https://www.rug.nl/ggdc/productivity/pwt"
-datasetDescription = "PWT version 10.0 is a database with information on relative levels of income, output, input and productivity, covering 183 countries between 1950 and 2019."
-datasetRetrieved = "XX June 2022"
-datasetNextUpdateExpected = "Unknown"
+# Specify dataset metadata - (this will end up in e.g. our database, 'Sources' tab etc.)
+dataset_meta = {
+    "name": "Penn World Tables version 10.0",
+    "sourceName": "Penn World Tables",
+    "link": "https://www.rug.nl/ggdc/productivity/pwt",
+    "description": "PWT version 10.0 is a database with information on relative levels of income, output, input and productivity, covering 183 countries between 1950 and 2019.",
+    "dateRetrieved": "dd/mm/2022",
+    "nextUpdate": "Unknown"
+}
+
 
 # %%
-# Print the metadata
-md("**Name:**  {}".format(datasetName))
-# %%
-md("**Source:**  {}".format(datasetSourceName))
-# %%
-md("**Link:**  {}".format(datasetLink))
-# %%
-md("**Description:**  {}".format(datasetDescription))
-# %%
-md("**Last updated:**  {}".format(datasetRetrieved))
-# %%
-md("**Expected data of next update:**  {}".format(datasetNextUpdateExpected))
+#Print the metadata as markdown
+md("**Name:**  {} <br><br>\
+    **Source:**  {} <br><br>\
+    **Link:**  {} <br><br>\
+    **Description:**  {} <br><br>\
+    **Last updated:**  {} <br><br>\
+    **Expected data of next update:**  {} "\
+    .format(dataset_meta["name"], 
+            dataset_meta["sourceName"],
+            dataset_meta["link"],
+            dataset_meta["description"],
+            dataset_meta["dateRetrieved"],
+            dataset_meta["nextUpdate"]))
+
+
 
 
 # %% [markdown]
@@ -108,7 +126,9 @@ df_original = pd.read_excel(tempf, sheet_name='Data')
 # %% [markdown]
 # Our World in Data standardizes country names to allow us to compare data across different data sources.
 # %%
-# Pablo: can you add the country harmonization step here
+# Pablo: can you add the country harmonization step here (df_harmonized is the df with standardized country names)
+
+df_harmonized = df_original
 
 
 # %%
@@ -120,33 +140,29 @@ df_original = pd.read_excel(tempf, sheet_name='Data')
 # %% [markdown]
 #The resulting data looks like this:
 # %%
-df_original.head()
+df_harmonized.head()
 
 # %% [markdown]
-# ## How we obtain each variable
+# ## A summary of each variable
 
 # *JH comment: See [Diana's Google Doc](https://docs.google.com/document/d/1Kg9ZqxXXfDWA7WxfDysB0GjwlQ6kK5x6kNP-m7Sjl-I/edit?pli=1#heading=h.3iglji7a4k32) for a previous attempt at this*
 
-# %%
-#Initialize empty arrays for variable-level metadata
-variableFinalNames = []
-variableDisplayNames = []
-variableUnitLongs = []
-variableUnitShorts = []
-variableDescription = []
-#This last one is the name we will use for this variable within this pipeline
-variableNames = []
 
 
 # %% [markdown]
 # ### GDP
 
+
 # %% [markdown]
+
 """
-*Joe will write here an explanation of the different approaches to prices that define the different GDP variables.*
+*JH comment: Joe is drafting here an explanation of the different approaches to prices that define the different GDP variables.*
 #
-Joe's notes: 
+The Penn World Tables provides five different measures of GDP over time.
 #
+They produce these different series... [explain about prices].
+#
+* Joe's notes:
 The point it if you want to compare across countries and over time then you can't have everything:*
 > "Diewert (1999) and Van Veelen (2002) have argued that no multilateral measure of real GDP can satisfy all the axioms we might like, so there are tradeoffs involved with any construction of this concept."
 #
@@ -167,8 +183,44 @@ PWT provide different measures for different purposes, in two different dimensio
    over time. `gdpna` applies NA growth rates (I don't yet get why this is different – 
    because pwt are using a different series for inflation than the NA GDP deflator?)
 
-"""
 
+You can read more about prices in our post here. (Joe to write this at some point in the future)
+
+
+"""
+# %% 
+# Prepare GDP data
+df_harmonized['rgdpe'] = df_original['rgdpe']*1000000
+df_harmonized['rgdpo'] = df_original['rgdpo']*1000000
+df_harmonized['cgdpe'] = df_original['cgdpe']*1000000
+df_harmonized['cgdpo'] = df_original['cgdpe']*1000000
+df_harmonized['rgdpna'] = df_original['rgdpna']*1000000
+
+
+# %%
+# Create a dictionary (of dictionaries)
+variable_meta = {}
+
+# Add to dictionary variable metadata - (this will end up in e.g. our database, 'Sources' tab etc.)
+variable_meta['rgdpe'] = {}
+
+variable_meta['rgdpe']['name'] = "Real GDP (expenditure-side)"
+variable_meta['rgdpe']['displayName'] = "Real GDP"
+variable_meta['rgdpe']['unitsLong'] = "International-$ at 2017 prices"
+variable_meta['rgdpe']['unitsShort'] = "$"
+variable_meta['rgdpe']['description'] = "[Long description here]"
+
+
+# %%
+#The original data is given in millions of dollars. We multiply the variable by 1,000,000 to give the figures in dollars.
+
+
+# %%
+df_harmonized['rgdpe'] = df_original['rgdpe']*1000000
+
+
+# %%
+display(HTML('<iframe src="https://ourworldindata.org/grapher/real-gdp-per-capita-PennWT" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>'))
 
 # %%
 # Append metadata for this variable to the arrays
@@ -194,11 +246,6 @@ variableDescription[len(variableDescription)-1]
 # **Original variable name within PWT:** rgdpe
 
 # %% [markdown]
-#The original data is given in millions of dollars. We multiply the variable by 1,000,000 to give the figures in dollars.
-
-# %%
-pwt10['rgdpe'] = df_original['rgdpe']*1000000
-# %% [markdown]
 # *“real GDP on
 # the output-side”, or real GDPo
 # , which is intended to measure the production possibilities of an
@@ -210,5 +257,15 @@ pwt10['rgdpe'] = df_original['rgdpe']*1000000
 # %% [markdown]
 # Divide by pop...
 
+
+# %% [markdown]
+## Appendix
+"""
+Here we provide links to further documentation of the Penn World Tables data 
+and our treatment of it. 
+#
+* A comparison of version 10.0 with a previous release of the data (JH comment: Pablo, at some point let's add this as an 'auxiliary notebook')
+* A list of further documentation discussing various vintages of the dataset can be found at the [Groningen Growth and Development Centre's website](https://www.rug.nl/ggdc/productivity/pwt/pwt-documentation). 
+"""
 
 # %%
