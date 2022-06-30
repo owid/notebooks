b7cbc322a577b98040018d22c836fd053201853c
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-06-30 10:25:40 +0100

    Adding documentation of GDP data

--- BetterDataDocs/PabloArriagada/pwt/pwt_pipeline.py
+++ BetterDataDocs/PabloArriagada/pwt/pwt_pipeline.py
@@ -18,8 +18,6 @@ from IPython.display import Markdown as md
 
 # ## About this data
 
-# *JH comment: My idea here is that we show here the metadata as in our database (and Sources tab). Possibly we could **specifiy** the metadata here? (We write it here, and then when you run the script it's these fields that end up in the database/grapher admin.
-
 # %%
 # Provide dataset metadata (as specified in Grapher Admin)
 datasetName = "Penn World Tables version 10.0"
@@ -31,12 +29,18 @@ datasetNextUpdateExpected = "Unknown"
 
 # %%
 # Print the metadata
-print("Name: " + datasetName)
-print("Source: " + datasetSourceName)
-print("Link: " + datasetLink)
-print("Description: " + datasetDescription)
-print("Last updated: " + datasetRetrieved)
-print("Expected data of next update: " + datasetNextUpdateExpected)
+md("**Name:**  {}".format(datasetName))
+# %%
+md("**Source:**  {}".format(datasetSourceName))
+# %%
+md("**Link:**  {}".format(datasetLink))
+# %%
+md("**Description:**  {}".format(datasetDescription))
+# %%
+md("**Last updated:**  {}".format(datasetRetrieved))
+# %%
+md("**Expected data of next update:**  {}".format(datasetNextUpdateExpected))
+
 
 # %% [markdown]
 # #### **All charts using this data**
@@ -86,7 +90,6 @@ import numpy as np
 md("#### Load our stored copy of the original data file (Retrieved on {})".format(datasetRetrieved))
 
 # %%
-#ETL note – This step is a bit like grabbing the data from Walden
 
 #Here we have original Excel file in GitHub
 url = 'https://raw.githubusercontent.com/owid/notebooks/main/PabloArriagada/pwt/data/pwt100.xlsx'
@@ -106,11 +109,7 @@ df_original = pd.read_excel(tempf, sheet_name='Data')
 # Our World in Data standardizes country names to allow us to compare data across different data sources.
 # %%
 # Pablo: can you add the country harmonization step here
-#ETL note – This step is a bit like grabbing the data from Walden
 
-
-
-df_final = df_original['countrycode', 'country']
 
 # %%
 # You can see a mapping of country names here:
@@ -118,18 +117,16 @@ df_final = df_original['countrycode', 'country']
 # Pablo: please provide a table of the country name mapping
 
 
+# %% [markdown]
+#The resulting data looks like this:
 # %%
 df_original.head()
+
 # %% [markdown]
-# ### Details of how we standardize the names of countries and world regions
-# %% [markdown]
-# *JH comment: Let's do this step here early on – as it would be in the etl process*
+# ## How we obtain each variable
 
-# ## Details of each variable we have prepared from Penn World Tables version 10
+# *JH comment: See [Diana's Google Doc](https://docs.google.com/document/d/1Kg9ZqxXXfDWA7WxfDysB0GjwlQ6kK5x6kNP-m7Sjl-I/edit?pli=1#heading=h.3iglji7a4k32) for a previous attempt at this*
 
-# *JH comment: I am adapting the text from [Diana's Google Doc](https://docs.google.com/document/d/1Kg9ZqxXXfDWA7WxfDysB0GjwlQ6kK5x6kNP-m7Sjl-I/edit?pli=1#heading=h.3iglji7a4k32)*
-
-# *JH comment: See my comment at the top of this doc about the Dataset-level metadata. Maybe we can define the variable-level metadata in a way that's helpful. For instance, below I make a set of ordered arrays that provide the metadata for for each variable. The idea is that this will be passed to the database/garpher admin. But we can also use to e.g. make the title of subsections. In that way the titles and the variable name will always be linked.*
 # %%
 #Initialize empty arrays for variable-level metadata
 variableFinalNames = []
@@ -137,28 +134,41 @@ variableDisplayNames = []
 variableUnitLongs = []
 variableUnitShorts = []
 variableDescription = []
+#This last one is the name we will use for this variable within this pipeline
 variableNames = []
+
+
 # %% [markdown]
 # ### GDP
+
 # %% [markdown]
-# *JH comment: Here let's explain the different approaches to prices that define the different GDP variables.*
+"""
+*Joe will write here an explanation of the different approaches to prices that define the different GDP variables.*
 #
-# *The point it if you want to compare across countries and over time then you can't have everything:*
-# *"Diewert (1999) and Van
-# Veelen (2002) have argued that no multilateral measure of real GDP can satisfy all the axioms
-# we might like, so there are tradeoffs involved with any construction of this concept."*
+Joe's notes: 
 #
-# *PWT provide different measures for different purposes, in two different dimensions:*
+The point it if you want to compare across countries and over time then you can't have everything:*
+> "Diewert (1999) and Van Veelen (2002) have argued that no multilateral measure of real GDP can satisfy all the axioms we might like, so there are tradeoffs involved with any construction of this concept."
 #
-# *1. Expenditure vs Output side*
-# *2. constant vs current prices vs NA growth rates
+PWT provide different measures for different purposes, in two different dimensions:
+
+ 1. Expenditure vs Production*
+ 2. Constant vs current prices vs NA growth rates
 #
-# *The issue with the (1) is – which prices do you look at?*
-# *The issue with (2) is How do you handle changing price structures over time if you want to compare across countries and over time?
-#
-#
-#
-#
+ * (1) is basically about the terms of trade: only if your terms of trade are especially favourable or especially bad will there be much difference between Expenditure and Output measures.
+ * The issue with (2) is this: Becasue price structures change over time, cross-country benchmarks 
+  for two periods typically won't be consistent with nationally measured 
+  inflation in each country. If you want to compare prices across place
+ and time, then you need to fudge this somehow. `rgdp(e/o)` is such a fudge:
+  They take the benchmark years as gospel and then make the national inflation 
+  fit somehow with that. `cgdp(e/o)` instead takes only the latest benchmark and
+  then applies national inflation. For this reason it is better for looking 
+  at trends in one country over time, but not so much for cross-country differences
+   over time. `gdpna` applies NA growth rates (I don't yet get why this is different – 
+   because pwt are using a different series for inflation than the NA GDP deflator?)
+
+"""
+
 
 # %%
 # Append metadata for this variable to the arrays
@@ -167,7 +177,7 @@ variableDisplayNames = np.append(variableDisplayNames,["Real GDP"])
 variableUnitLongs = np.append(variableUnitLongs,["International-$ at 2017 prices"])
 variableUnitShorts = np.append(variableUnitShorts,["$"])
 variableDescription = np.append(variableDescription,["[Joe to write a description of this variable]"])
-
+variableNames = ['final_rgdpe']
 # %%
 # Print display name of current variable. It would be great if we could figure out a way to render this output as a html heading...
 
