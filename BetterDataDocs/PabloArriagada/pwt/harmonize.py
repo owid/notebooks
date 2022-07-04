ef15e1dfd0c6ab2bb7221929b0ba3de1bc882d69
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-04 21:20:20 +0100

    Adjustments to make it work better in Colabs

--- BetterDataDocs/PabloArriagada/pwt/harmonize.py
+++ BetterDataDocs/PabloArriagada/pwt/harmonize.py
@@ -8,13 +8,13 @@
 This script is part of a series documenting how we prepare the data 
 provided in the Penn World Tables for use in our website and charts.
 
-<br><br>
+<br>
 
 We make these scripts available in two formats: as scripts stored 
 [here in GitHub](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit), 
 and as notebooks published in Google Colabs.
 
-<br><br>
+<br>
 
 If you have this open as a notebook, for instance in 
 Google Colabs, you can run the code below and also edit it to explore the data.
@@ -26,13 +26,13 @@ Google Colabs, you can run the code below and also edit it to explore the data.
 """
 ### Data preparation steps
 
-* **Step 1) Standardize country names**   (Current document) – [View in GitHub](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit)
+* **Step 1. Standardize country names**   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
 <br>
-*In this notebook we document how we load the original raw data file and standardize
-the country names.*
-<br><br>
-* Step 2) Write metadata   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
-* Step 3) Prepare and transform variables   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
+In this notebook we document how we load the original raw data file and standardize
+the country names.
+<br>
+* Step 2. Write metadata   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
+* Step 3. Prepare and transform variables   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
 
 """
 
@@ -42,22 +42,20 @@ the country names.*
 """
 ## Set up and permissions
 
-In this section we load any modules or packages we need to run the code.
-<br><br>
-Prior to this, we run a check to see whether you are viewing this in Google Colabs and, if so, 
-install any needed packages.
-<br><br>
-This section also includes code to allow us (the Our World in Data team) to 
-upload data to our database once it's been prepared. The code below first checks whether you have 
-the right permissions to upload to our database, and, if not, sections of the code below related to 
-this will not run. This should not affect your ability 
-to run the rest of the code or add/edit cells to explore the data.
+This section needs to be run in order to load packages used.
+<br>
+If you are viewing this in Colabs, it will also install any packages not pre-installed 
+in this environment.
+<br>
+In addition, we run code to allow us (the Our World in Data team) to 
+upload data to our database once it's been prepared. Unless you are running this with the
+corrent access keys, code blocks relating to data uploads will not run.
 """
 
-# %% [markdown]
-# Check if in Colabs, and if so install needed packages. 
 # %%
-# Test if notebook is running on Google Colab, by trying to load a colab-specific package.
+#@title Run set up
+
+# Test if this script is running on Google Colab, by trying to load a colab-specific package.
 
 try:
   import google.colab
@@ -65,16 +63,16 @@ try:
 except:
   IN_COLAB = False
 
-# %%
-# Install needed packages
+
+# If this is in Colabs, install any needed packages
 #if IN_COLAB:
 # None to install
 
 
-# %% [markdown]
-# Set up to upload data our database, if correct permissions
 
-# %%
+# If the correct access keys are in place, set up a s3 client and session 
+# to be able to upload to our database.
+
 s3access = True
 
 try:
@@ -87,7 +85,6 @@ try:
         # A function we have written to help upload our data to our s3 cloud storage
         from functions import upload_to_s3
 
-
         # Set up access for writing files to s3  
         session = boto3.session.Session()
 
@@ -99,57 +96,89 @@ try:
 except:
         s3access = False
 
-# %% [markdown]
-# Load packages.
 
- # %%
+
+# Load packages
+
 # Pandas is a standard package used for data manipulation in python code
 import pandas as pd
                
 
 
+# %% [markdown]
+## Load data and standardize country names
 
-# ------- RUN –––––––––
+### Load original data file
 
 # %%
-# Load the 'Walden' data (for the time being in Joe's s3) 
+# Load the original raw data file as a pandas dataframe. 
+# (For the time being, this is in Joe's own Digital Ocean account).
 
 url = 'https://joeh.fra1.digitaloceanspaces.com/pwt/pwt100-original.xlsx'
 
 df_original = pd.read_excel(url, sheet_name='Data')
 
+# %% [markdown]
+# Here is the first few rows of the data:
+# %%
+df_original.head()
+
+# %% [markdown]
+## Standardize country names
+
+# %% [markdown]
+# We adjust the country names to align with our standardized set of names. This makes it 
+# possible for us compare data across different datasets.
 
 # %%
-# Standardize country names
-
-# %%
-# Read in mapping csv – This needs to be two columns headed 'Original Name' and 'Our World In Data Name'
+# Read in mapping table which maps PWT names onto OWID names.
 url = 'https://joeh.fra1.digitaloceanspaces.com/pwt/country_standardization_mapping.csv'
 
 df_mapping = pd.read_csv(url)
 
+
+# %% [markdown]
+# Here is the first few rows of the mapping table:
 # %%
-# Merge in the mapping to the main df
+df_mapping.head()
+
+# %%
+# Merge in the mapping to the main dataframe and tidy
+
+
+# Merge
 df_harmonized = pd.merge(df_original,df_mapping,
     left_on='country',right_on='Original Name', how='left')
     
 # Drop the old entity names column, and the matching column from the mapping file
 df_harmonized = df_harmonized.drop(columns=['country', 'Original Name'])
     
-# Rename the new entity col as specefied in the args
+# Rename the new entity column
 df_harmonized = df_harmonized.rename(columns={'Our World In Data Name': 'country'})
-    
-# Move entity variable to first column
+
+# Move the country column to front:
+
 # get a list of columns
 cols = list(df_harmonized)
     
-# move the column to head of list using index, pop and insert
+# move the country column to the first in the list of columns
 cols.insert(0, cols.pop(cols.index('country')))
     
-# use loc to reorder
+# reorder the columns of the dataframe according to the list
 df_harmonized = df_harmonized.loc[:, cols]
 
+# %% [markdown]
+# Here is the first few rows of the data after swapping the country names:
+# %%
+df_harmonized.head()
 
+
+# %% [markdown]
+## Upload prepared data to our database
+
+"""
+This section is for internal purposes, and will not run unless you have the right permissions.
+"""
 # %%
 if s3access:
     upload_to_s3(df_harmonized, 'pwt', 'harmonized.csv')
