136f88ee08439379a53751261f95b5a108e45670
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-05 11:48:05 +0100

    Format prep_vars script to work better in colabs

--- BetterDataDocs/PabloArriagada/pwt/prepare_variables.py
+++ BetterDataDocs/PabloArriagada/pwt/prepare_variables.py
@@ -1,62 +1,144 @@
-# %%
+
+# %% [markdown]
+"""
+# How does Our World in Data prepare data from the Penn World Tables?
+"""
+# %% [markdown]
+"""
+### About this document
+This script is part of a series documenting how we prepare the data 
+provided in the Penn World Tables for use in our website and charts.
+
+<br>
+
+We make these scripts available in two formats: as scripts stored 
+[here in GitHub](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit), 
+and as notebooks published in Google Colabs.
+
+<br>
+
+If you have this open as a notebook, for instance in 
+Google Colabs, you can run the code below and also edit it to explore the data.
+
+
+"""
+# %% [markdown]
+"""
+### Data preparation steps
+
+* **Step 1. Standardize country names**   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
+<br>
+In this notebook we document how we load the original raw data file and standardize
+the country names.
+<br>
+* Step 2. Write metadata   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
+* Step 3. Prepare and transform variables   ([Open in Colabs](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit) – [View in Github](https://docs.google.com/document/d/1VDWq2JggspDPyFjLg47DsIqNuFXAaE4SO8_CeMLMqVk/edit))
+"""
+
+# %% [markdown]
+"""
+## Set up and permissions
+
+This section needs to be run in order to load packages used.
+<br>
+If you are viewing this in Colabs, it will also install any packages not pre-installed 
+in this environment.
+<br>
+In addition, we run code to allow us (the Our World in Data team) to 
+upload data to our database once it's been prepared. Unless you are running this with the
+corrent access keys, code blocks relating to data uploads will not run.
+"""
 
 # %%
-# Pandas is a standard package used for data manipulation in python code
-import pandas as pd
+#@title Run set up
 
-# boto3  allows us to write data to our s3 cloud storage
-import boto3
+# Test if this script is running on Google Colab, by trying to load a colab-specific package.
 
-# Keys for accessing s3
-from joes_key import KEY_ID, SECRET_KEY 
-
-# A function we have written to help upload our data to our s3 cloud storage
-from functions import upload_to_s3
+try:
+  import google.colab
+  IN_COLAB = True
+except:
+  IN_COLAB = False
 
 
-# Note, we should grab this from the etl instead when it's ready
-from variable_metadata import variable_meta
+# If this is in Colabs, install any needed packages
+#if IN_COLAB:
+# None to install
 
 
-# %%
-# Set up access for writing files to s3  
-session = boto3.session.Session()
 
-client = session.client('s3',
+# If the correct access keys are in place, set up a s3 client and session 
+# to be able to upload to our database.
+
+s3access = True
+
+try:
+        # Acess keys to write to  our s3 cloud storage
+        from joes_key import KEY_ID, SECRET_KEY 
+
+        # boto3  allows us to write data to our s3 cloud storage
+        import boto3
+
+        # A function we have written to help upload our data to our s3 cloud storage
+        from functions import upload_to_s3
+
+        # Set up access for writing files to s3  
+        session = boto3.session.Session()
+
+        client = session.client('s3',
                         endpoint_url="https://joeh.fra1.digitaloceanspaces.com",
                         aws_access_key_id=KEY_ID,
                         aws_secret_access_key=SECRET_KEY)
 
+except:
+        s3access = False
 
 
 
-# ------- LOAD –––––––––
+# Load packages
+
+# Pandas is a standard package used for data manipulation in python code
+import pandas as pd
+               
+
+
+# %% [markdown]
+# # Load data
 
 # %%
-# Load the 'harmonized' data (i.e with standardized country names)
+# Loading the 'harmonized' data (i.e with standardized country names)
 # – For the time being, this is stored in Joe's Digital Ocean account.
 
 url = 'https://joeh.fra1.digitaloceanspaces.com/pwt/harmonized.csv'
 
 df = pd.read_csv(url)
 
+# %% [markdown]
+# Here is the first few rows of the data:
+# %%
+df.head()
 
 
+# %% [markdown]
+# # Prepapre variables for Our World in Data
 
-# ------- RUN –––––––––
-
-# –––– Construct absolute GDP variables –––––
-#The original data is given in millions of dollars.
+# %% [markdown]
+# ## Absolute GDP variables
+# The original data is given in millions of dollars.<br>
 # We multiply the variable by 1,000,000 to give the figures in dollars.
+# %%
 df['rgdpe'] = df['rgdpe']*1000000
 df['rgdpo'] = df['rgdpo']*1000000
 df['cgdpe'] = df['cgdpe']*1000000
 df['cgdpo'] = df['cgdpo']*1000000
 df['rgdpna'] = df['rgdpna']*1000000
 
-
-# –––– Construct GDP per capita variables –––––
-# GDP per capita is GDP divided by popultion (both are given in millions)
+# %% [markdown]
+# ## GDP per capita variables
+# Penn World Tables do not directly provide GDP per capita. We calculate
+# these by dividing GDP by the population figures they provide (both
+# given in millions).
+# %%
 df['rgdpe_pc'] = df['rgdpe']/df['pop']
 df['rgdpo_pc'] = df['rgdpo']/df['pop']
 df['cgdpe_pc'] = df['cgdpe']/df['pop']
