f8f417586755a4e44c8eb7d9200c278e433c6109
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-07 17:22:28 +0100

    Add countrycode useage to standardization mapping

--- BetterDataDocs/PabloArriagada/pwt/main.py
+++ BetterDataDocs/PabloArriagada/pwt/main.py
@@ -1,23 +1,28 @@
 
-import make_dataframe
+#import make_dataframe
 
 from standardize_entities import standardize_and_save
+
 
 # Standardize entity names in main data file
 standardize_and_save(
     raw_csv_url="https://joeh.fra1.digitaloceanspaces.com/pwt/raw_dataframe.csv",
     entity_mapping_url= "https://joeh.fra1.digitaloceanspaces.com/pwt/country_standardization_mapping.csv",
-    entity_name_in_raw='country',
-    resulting_entity_name='entity',
+    mapping_varname_raw = 'Original Name',
+    mapping_vaname_owid = "Our World In Data Name",
+    data_varname_old='country',
+    data_varname_new='entity',
     s3_space_to_save_in='pwt',
     as_filename='entities_standardized.csv'
                     )
 
-# Standardize entity names in national accounts data file
+#Standardize entity names in national accounts data file
 standardize_and_save(
     raw_csv_url="https://joeh.fra1.digitaloceanspaces.com/pwt/raw_dataframe_national_accounts.csv",
-    entity_mapping_url= "https://joeh.fra1.digitaloceanspaces.com/pwt/raw_dataframe_national_accounts.csv",
-    entity_name_in_raw='country',
+    entity_mapping_url= "https://joeh.fra1.digitaloceanspaces.com/pwt/country_standardization_mapping.csv",
+    mapping_varname_raw = 'Original Name',
+    mapping_vaname_owid = "Our World In Data Name",
+    entity_name_in_raw='countrycode',
     resulting_entity_name='entity',
     s3_space_to_save_in='pwt',
     as_filename='entities_standardized_national_accounts.csv'
@@ -26,3 +31,5 @@ standardize_and_save(
 import prepare_variables
 
 import write_metadata
+
+
--- BetterDataDocs/PabloArriagada/pwt/noteboooks/analysis_notebooks/compare_NA_and_main_data.py
+++ BetterDataDocs/PabloArriagada/pwt/noteboooks/analysis_notebooks/compare_NA_and_main_data.py
@@ -2,5 +2,22 @@
 
 
 
+# %%
+import pandas as pd
 
-url = "https://joeh.fra1.digitaloceanspaces.com/pwt/country_standardization_mapping.csv"
+# %%
+#Main data file
+url = "https://joeh.fra1.digitaloceanspaces.com/pwt/entities_standardized_national_accounts.csv"
+
+df_main = pd.read_csv(url)
+
+#%%
+df_main.head()
+
+#%%
+# National Accounts data file
+url = "https://joeh.fra1.digitaloceanspaces.com/pwt/entities_standardized.csv"
+
+df_na = pd.read_csv(url)
+#%%
+df_na.head()
