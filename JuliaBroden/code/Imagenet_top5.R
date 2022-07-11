bfe5923d691db9007348595002c08a8f6d5ffeb2
Julia Broden <58789198+jsbroden@users.noreply.github.com>
2022-07-11 13:12:32 +0200

    Add Imagenet top5 code

---
+++ JuliaBroden/code/Imagenet_top5.R
@@ -0,0 +1,17 @@
+rm(list = ls())
+library(stringr)
+library(googlesheets4)
+library(dplyr)
+library(readr)
+
+sheet_url <- "https://docs.google.com/spreadsheets/d/1C1aJ9TVVCYDvRHsf3i8fhFCm7UhjqO1J7AWoSXyM--U/edit#gid=586400223"
+
+df <- read_sheet(sheet_url, sheet = 2)
+
+df <- df %>% 
+  select(-c(Method,Source)) %>%
+  rename(Entity = `Type`) %>%
+  mutate(Entity = str_to_sentence(Entity)) %>%
+  relocate(Entity, Year)
+
+write_csv(df, "Imagenet_top5.csv")
