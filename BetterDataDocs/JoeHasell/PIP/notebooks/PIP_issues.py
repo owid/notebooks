7c58ee9eee090f915b5c2a8b8f908b10fdde650e
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-12 16:04:08 +0100

    New script for pulling together available deciles

--- BetterDataDocs/JoeHasell/PIP/notebooks/PIP_issues.py
+++ BetterDataDocs/JoeHasell/PIP/notebooks/PIP_issues.py
@@ -6,3 +6,6 @@
 
 
 #%%
+# Note: distributional data (median, Dini, deciles etc.) are missing for ~2000 rows,
+#  without it being clear why or what the patten is. For instnce Angola in 2000 yes, but 2001 no. 
+# Perhaps something to do with interpolation vs extrpolation
