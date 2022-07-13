f3eb92b3c9b3da9ab620479dc7d3d2396e79159e
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-14 00:28:49 +0100

    Copy over relevant files from other project

---
+++ BetterDataDocs/JoeHasell/PhD_global_dist/API_query_lists.py
@@ -0,0 +1,4 @@
+
+
+#Specify the list of percintiles to query...
+percentiles = range(1,100, 1)
---
+++ BetterDataDocs/JoeHasell/PhD_global_dist/percentiles_request.py
@@ -0,0 +1,35 @@
+
+#%%
+import pandas as pd
+import gc
+
+from API_query_lists import percentiles
+
+#%%
+
+for p in percentiles:
+    print(f"Fetching P{p}")
+    popshare = p/100
+
+
+    # 'filled'/'lined up' data – countries
+
+    request_url = f'https://api.worldbank.org/pip/v1/pip?country=all&year=all&popshare={popshare}&fill_gaps=true&welfare_type=all&reporting_level=all&format=csv'
+
+    df_filled = pd.read_csv(request_url)
+
+    df_filled = df_filled.rename(columns={'country_name':'entity', 
+                        'reporting_year':'year'})
+
+    df_filled = df_filled[['entity', 'year', 'reporting_level', 'welfare_type', 'poverty_line','headcount']]
+
+    df_filled['requested_p'] = p
+
+    # Write to .CSV
+    df_filled.to_csv(f'data/PIP_percentiles/P{p}.csv', index=False)
+
+
+    # Drop from memory
+    del df_filled
+    gc.collect()
+#%%
