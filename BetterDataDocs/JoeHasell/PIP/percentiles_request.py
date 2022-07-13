f7bc33204650b6bab30360a34e33b9be644b09b6
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-13 15:56:58 +0100

    Add sense check notebook

--- BetterDataDocs/JoeHasell/PIP/percentiles_request.py
+++ BetterDataDocs/JoeHasell/PIP/percentiles_request.py
@@ -11,23 +11,6 @@ for p in percentiles:
     print(f"Fetching P{p}")
     popshare = p/100
 
-    # # survey estimates only
-    # request_url = f'https://api.worldbank.org/pip/v1/pip?country=all&year=all&popshare={popshare}&fill_gaps=false&welfare_type=all&reporting_level=all&format=csv'
-
-    # df_survey = pd.read_csv(request_url)
-
-    # # Note we round survey year to nearest integer year
-    # df_survey['year'] = round(df_survey['survey_year'])
-
-    # df_survey = df_survey.rename(columns={'country_name':'entity', 
-    #                 'poverty_line':'threshold'})
-
-    # df_survey = df_survey[['entity', 'year', 'threshold','headcount']]
-
-    # # Write to .CSV
-    # df_survey.to_csv(f'API_output/percentiles/survey_data/P{p}.csv')
-
-
 
     # 'filled'/'lined up' data – countries
 
@@ -36,10 +19,9 @@ for p in percentiles:
     df_filled = pd.read_csv(request_url)
 
     df_filled = df_filled.rename(columns={'country_name':'entity', 
-                        'reporting_year':'year', 
-                        'poverty_line':'threshold'})
+                        'reporting_year':'year'})
 
-    df_filled = df_filled[['entity', 'year', 'threshold','headcount']]
+    df_filled = df_filled[['entity', 'year', 'poverty_line','headcount']]
 
     df_filled['requested_p'] = p
 
