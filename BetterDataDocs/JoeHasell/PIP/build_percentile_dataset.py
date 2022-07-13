84a62cb5136c73c496e5acd167e98f6ab32f6ad0
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-13 20:21:49 +0100

    adjust headers and indexes

--- BetterDataDocs/JoeHasell/PIP/build_percentile_dataset.py
+++ BetterDataDocs/JoeHasell/PIP/build_percentile_dataset.py
@@ -9,12 +9,15 @@ all_files = Path('API_output/percentiles/filled_data').glob('*.csv')
 
 # Read in and concat (append together) all the files
 df = pd.concat((pd.read_csv(f) for f in all_files))
-# %%
+
+#%%
 
 # Write to .CSV
-df.to_csv(f'API_output/percentiles/all_percentiles.csv', index=False)
+df.to_csv('API_output/percentiles/all_percentiles.csv', index=False)
 #%%
 
 # Cleaning - see notebook for discussion
 
 # Drop headcount=0 observations
+# Drop El Salvador and Sierra Leone (negative poverty lines)
+
