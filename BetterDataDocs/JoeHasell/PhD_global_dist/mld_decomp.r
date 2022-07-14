82e1fe2df7ce42f9ec7e3ae0eeb83ebcdc9e6644
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-14 02:25:09 +0100

    Add to decomp

--- BetterDataDocs/JoeHasell/PhD_global_dist/mld_decomp.r
+++ BetterDataDocs/JoeHasell/PhD_global_dist/mld_decomp.r
@@ -1,11 +1,13 @@
 library(gpinter)
 library(tidyverse)
+library(dineq)
 
 
-url = "https://joeh.fra1.digitaloceanspaces.com/phd_global_dist/percentiles_from_PIP.csv"
 
 
-df<- read.csv(url)
+
+
+df<- read.csv('data/clean_percentiles.csv')
 
 head(df)
 
@@ -28,7 +30,36 @@ filter(number_of_reporting_levels<3 & reporting_level!='national') %>%
 select(entity) %>%
 unique()
 
-# MLD decomposition
+# Keep only national estimates except for these cases
+
+df<- df %>%
+filter(reporting_level=='national' | number_of_reporting_levels<3)
+
+# Check again if there is more than one reporting level
+df %>%
+ count(entity, year, p) %>%
+ rename(number_of_reporting_levels = n) %>%
+    filter(number_of_reporting_levels>1)
+
+
+
+
+# ### Check coverage
+
+country_list<- unique(df$entity)
+
+df %>%
+groupby(year) %>%
+ungroup()
+
+# ### MLD decomposition
+
+# Calculate population in each percentile
+
+df<- df %>%
+ mutate(pop_in_perc = share_of_pop * reporting_pop)
+
+year_list = as.numeric(unique(df$year))
 
 # +
 #
@@ -46,8 +77,8 @@ for(y in year_list){
     filter(year == y)
   
  mld_results<- mld_decomp(df_this_year$average_in_bracket,
-                   df_this_year$country, 
-                   weights = brackets_avgs_this_year$share_of_pop)
+                   df_this_year$entity, 
+                   weights = df_this_year$pop_in_perc)
 
  mld_decomp_this_year<- data.frame(
               year = y,
@@ -66,3 +97,57 @@ mld_decomp<- mld_decomp %>%
   mutate(within_share = mld_within/mld_total,
          between_share = mld_between/mld_total,
          )
+# -
+
+mld_decomp
+
+# ### Alternative scenarios
+
+# #### Set top 1% share to 20% universally
+
+# +
+# Function to replace average income above a threshold with the average that would yield a given (higher) top income share.
+
+# Arguments:
+  # Thr: percentile above which to adjust the distribution
+  # A_t_1: original average above percentile
+  # S_t_1: original share above percentile
+  # S_adj_t_1: target adjusted share above percentile
+  # A_0_1: original overall average
+
+# For the derivation see here: https://imgur.com/a/O1Ko5jT
+
+adjusted_averages<- function(Thr, A_t_1, S_t_1, S_adj_t_1, A_0_1){
+
+
+# Ratio of adjusted to unadjusted top share
+K = S_adj_t_1/S_t_1
+
+# Calculate original average below percentile (reverse the weighted average)
+A_0_t <- (A_0_1 - (1-Thr) * A_t_1) / Thr
+
+
+# Calculate adjusted average above percentile
+A_adj_t_1 = (Thr * A_0_t)/(A_0_1/(K * A_t_1)-(1-Thr)) 
+
+# Calculate adjusted overall average (weighted average)
+A_adj_0_1 = Thr * A_0_t + (1-Thr) * A_adj_t_1
+
+# outputs<- list()
+# outputs[["Adjusted top average"]]<- A_adj_t_1
+# outputs[["Adjusted overall average"]]<- A_adj_0_1
+
+return(A_adj_t_1)
+
+}
+
+
+check<- adjusted_averages(0.9, 30, 10, 20, 10)
+
+check
+# -
+
+head(df)
+
+# df_alt_20<- df %>%
+#   mutate(alt_average_in_bracket = if_else()
