e4ce0fad4d24d7f0b5b95e46e9cfda5e6727e508
parriagadap <paarriagadap@gmail.com>
2022-07-18 18:45:07 -0400

    test percent script

---
+++ BetterDataDocs/PabloArriagada/percent test/percent test.py
@@ -0,0 +1,12 @@
+# %% [markdown]
+# # This is separated
+# Still separated
+# %%[markdown]
+# # This is together
+# Still together
+
+# %% [markdown]
+# # I created this one with Jupyter Notebook
+# Still Jupyter Notebook
+
+# %%
