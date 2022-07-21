a976de72bef6dc9548b6119eeb2713f04f685ff6
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-22 00:27:51 +0300

    Merge wid and pip data and restructure project

--- BetterDataDocs/JoeHasell/PhD_global_dist/compare_wid_pip_gini_and_topshares.r
+++ BetterDataDocs/JoeHasell/PhD_global_dist/compare_wid_pip_gini_and_topshares.r
@@ -1,64 +1,65 @@
 # -*- coding: utf-8 -*-
 library(tidyverse)
 
-# Pull in PIP data
+# Pull in WID-PIP data
 
-df_pip<- read.csv("https://joeh.fra1.digitaloceanspaces.com/PIP/main_data.csv")
-
-nrow(df_pip)
-
-# Select only one reporting_level and welfare_type per entity-year (so that entity-years become unique)
-
-# +
-#If there are multiple reporting_levels for an entity-year, keep only national
-df_pip<- df_pip %>% 
-  group_by(entity, reporting_year, welfare_type) %>% 
-  mutate(flag = +(n() > 1)) %>% 
-  ungroup() %>%
-  filter(flag==0 | reporting_level == "national") %>%
-    select(-flag)
+df<- read.csv("https://joeh.fra1.digitaloceanspaces.com/phd_global_dist/wid_pip_gini_and_topshares.csv")
 
 
-
-# -
-
-nrow(df_pip)
-
-#If there are multiple welfare_types for an entity-year, keep only consumption
-    #...In fact there are none
-df_pip<- df_pip %>% 
-  group_by(entity, reporting_year, reporting_level) %>% 
-  mutate(flag = +(n() > 1)) %>% 
-  ungroup() %>%
-  filter(flag==0 | welfare_type == "consumption") %>%
-    select(-flag)
-
-nrow(df_pip)
-
-# Pull in WID data
-
-df_wid<- read.csv("https://joeh.fra1.digitaloceanspaces.com/wid/gini_and_topshares_standardized.csv")
-
-head(df_wid)
-
-# Merge the two data frames
-
-df<- full_join(df_wid, df_pip,  by = c("entity" = "entity", "year" = "reporting_year"))
-
-head(df)
-
-names(df)
+# ## Charts
 
 # Calculate change in Gini 1990 – 2019
 
-# +
-gini_change<- df %>% filter(year %in% c(1990, 2019)) %>% 
+ineq_change<- df %>% filter(year %in% c(1990, 2019)) %>% 
                 select(entity, year, wid_gini, gini, wid_top_10_share, shares_decile_10, wid_top_1_share, shares_top_1pc) %>%
-                pivot_wider(names_from = year, values_from = c(wid_gini, gini, wid_top_10_share, shares_decile_10, wid_top_1_share, shares_top_1pc), names_sep = "_") 
+                pivot_wider(names_from = year, values_from = c(wid_gini, gini, wid_top_10_share, shares_decile_10, wid_top_1_share, shares_top_1pc), names_sep = "_") %>%
+                mutate(wid_gini_change = wid_gini_2019 - wid_gini_1990,
+                       pip_gini_change = gini_2019 - gini_1990,
+                       wid_top10share_change = wid_top_10_share_2019 - wid_top_10_share_1990,
+                       pip_top10share_change = shares_decile_10_2019 - shares_decile_10_1990,
+                       wid_top1share_change = wid_top_1_share_2019 - wid_top_1_share_1990,
+                       pip_top1share_change = shares_top_1pc_2019 - shares_top_1pc_1990)
 
-gini_change
+
+ggplot(ineq_change, aes(x = pip_gini_change, y = wid_gini_change)) +
+       geom_point() +            
+        geom_abline(intercept = 0,
+              slope = 1)
+
+# +
+change_threshold = 0.01
+
+ineq_change %>%
+    select(entity, wid_gini_change, pip_gini_change) %>%
+    drop_na() %>%
+    summarize(n_entity = n(),
+              n_fall_wid = sum(wid_gini_change < -change_threshold),
+              n_fall_pip = sum(pip_gini_change < -change_threshold),
+              n_rise_wid = sum(wid_gini_change > change_threshold),
+              n_rise_pip = sum(pip_gini_change > change_threshold))
 # -
 
-df %>% filter(entity=="")
+ggplot(ineq_change, aes(x = pip_top10share_change, y = wid_top10share_change)) +
+       geom_point() +            
+        geom_abline(intercept = 0,
+              slope = 1)
+
+# +
+change_threshold = 0.03
+
+ineq_change %>%
+    select(entity, wid_top10share_change, pip_top10share_change) %>%
+    drop_na() %>%
+    summarize(n_entity = n(),
+              n_fall_wid = sum(wid_top10share_change < -change_threshold),
+              n_fall_pip = sum(pip_top10share_change < -change_threshold),
+              n_rise_wid = sum(wid_top10share_change > change_threshold),
+              n_rise_pip = sum(pip_top10share_change > change_threshold))
+# -
+
+ggplot(ineq_change, aes(x = pip_top1share_change, y = wid_top1share_change)) +
+       geom_point() +            
+        geom_abline(intercept = 0,
+              slope = 1)
 
 
