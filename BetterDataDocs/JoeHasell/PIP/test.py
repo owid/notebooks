c745418856018e5b53593b5e2d6de5656527582a
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-22 01:48:24 +0300

    testing API

---
+++ BetterDataDocs/JoeHasell/PIP/test.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+
+#%%
+
+df_p90_filled = pd.read_csv("data/API_output/percentiles/filled_true/P90.csv")
+
+df_p10_filled = pd.read_csv("data/API_output/percentiles/filled_true/P10.csv")
+
+# %%
+"
