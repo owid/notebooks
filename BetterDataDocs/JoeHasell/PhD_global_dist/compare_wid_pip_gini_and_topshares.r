dd3f787f4d869d246c3f7366c211941e2de28cdc
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-21 22:40:13 +0300

    compare WID and PIP data

--- BetterDataDocs/JoeHasell/PhD_global_dist/compare_wid_pip_gini_and_topshares.r
+++ BetterDataDocs/JoeHasell/PhD_global_dist/compare_wid_pip_gini_and_topshares.r
@@ -1,9 +1,66 @@
+# -*- coding: utf-8 -*-
 library(tidyverse)
 
 # Pull in PIP data
 
-df_pip<- read.csv("https://joeh.fra1.digitaloceanspaces.com/wid/gini_and_topshares_standardized.csv")
+df_pip<- read.csv("https://joeh.fra1.digitaloceanspaces.com/PIP/main_data.csv")
+
+nrow(df_pip)
+
+# Select only one reporting_level and welfare_type per entity-year (so that entity-years become unique)
+
+# +
+#If there are multiple reporting_levels for an entity-year, keep only national
+df_pip<- df_pip %>% 
+  group_by(entity, reporting_year, welfare_type) %>% 
+  mutate(flag = +(n() > 1)) %>% 
+  ungroup() %>%
+  filter(flag==0 | reporting_level == "national") %>%
+    select(-flag)
 
 
+
+# -
+
+nrow(df_pip)
+
+#If there are multiple welfare_types for an entity-year, keep only consumption
+    #...In fact there are none
+df_pip<- df_pip %>% 
+  group_by(entity, reporting_year, reporting_level) %>% 
+  mutate(flag = +(n() > 1)) %>% 
+  ungroup() %>%
+  filter(flag==0 | welfare_type == "consumption") %>%
+    select(-flag)
+
+nrow(df_pip)
+
+# Pull in WID data
 
 df_wid<- read.csv("https://joeh.fra1.digitaloceanspaces.com/wid/gini_and_topshares_standardized.csv")
+
+head(df_wid)
+
+# Merge the two data frames
+
+df<- full_join(df_wid, df_pip,  by = c("entity" = "entity", "year" = "reporting_year"))
+
+head(df)
+
+summary(df)
+
+df %>% filter(year %in% c(1990, 2019)) %>% select(entity, year, wid_gini, gini) %>% drop_na() %>% count(entity)
+
+# Calculate change in Gini 1990 – 2019
+
+# +
+gini_change<- df %>% filter(year %in% c(1990, 2019)) %>% 
+                select(entity, year, wid_gini, gini) %>%
+                pivot_wider(id_Cols = entity, names_from = year, values_from = c(wid_gini, gini), names_sep = "_")
+
+gini_change
+# -
+
+df %>% filter(entity=="")
+
+
