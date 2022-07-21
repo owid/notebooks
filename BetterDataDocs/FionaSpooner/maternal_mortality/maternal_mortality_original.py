4e3cec6a06b30adb6ce6243f67ea2cd2e1853c01
spoonerf <spoonerfiona@gmail.com>
2022-07-21 13:53:58 +0100

    refactor(mmr): adding oecd and checking mmr calculations

--- BetterDataDocs/FionaSpooner/maternal_mortality/maternal_mortality_original.py
+++ BetterDataDocs/FionaSpooner/maternal_mortality/maternal_mortality_original.py
@@ -9,7 +9,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.11.2
 #   kernelspec:
-#     display_name: 'Python 3.10.4 (''venv'': venv)'
+#     display_name: Python 3.10.4 64-bit ('3.10.4')
 #     language: python
 #     name: python3
 # ---
@@ -122,7 +122,8 @@ df_d.loc[
 ] = 1957
 
 # Drop any rows where MMR is zero or '...' (Ireland, Denmark and Sri Lanka)
-df_d = df_d.drop(df_d[(df_d["MMR"] == 0) | (df_d["MMR"] == '...')].index)
+df_d = df_d.drop(df_d[(df_d["MMR"] == 0) | (df_d["MMR"] == "...")].index)
+
 
 df_d = df_d.drop(
     df_d[
@@ -144,7 +145,7 @@ df_d = df_d.drop(
 
 # %%
 assert (
-    df_d[df_d.duplicated(subset = ["Country", "year"],keep=False)].shape[0]
+    df_d[["Country", "year"]][df_d[["Country", "year"]].duplicated(keep=False)].shape[0]
     == 0
 )
 
@@ -152,6 +153,26 @@ assert (
 # ## Finding the mid-point of each year range
 
 # For some rows a range of years is given for a particular maternal mortality rate. We want to find the mid-point of that range. We use this function to find the mid-value of years given.
+
+
+# %%
+df_d.columns
+
+# %% [markdown]
+# Checking the given MMR matches a calculated MMR, where the data for live births and maternal deaths is given. It seems largely okay but the values for Ireland, particularly from 1901-1920. There is a drop of almost 1000 maternal deaths between 1920 and 1922. 
+#
+# In the registrar general of Ireland it states that there were approximately 599 maternal deaths in Ireland in 1912 and on average 634 per year between 1903 and 1911. The estimated rate is 6 deaths per 1,000 births, so 600 per 100,000 births.
+#
+# I think this suggests that the Maternal deaths figures may be wrong for the period 1901-1920.  
+
+# %%
+df_check = df_d[['Country','year','Live Births', 'Maternal deaths', 'MMR']].dropna().reset_index(drop=True)
+df_check['mmr_calc'] = ((df_check['Maternal deaths']/df_check['Live Births'])* 100000).astype(float).round(2)
+df_check['MMR'] = df_check['MMR'].astype(float).round(2)
+
+
+df_check[ df_check['mmr_calc'].round(2)!= df_check['MMR'].round(2)]
+
 
 
 # %%
@@ -210,7 +231,7 @@ df_gap.rename(
 
 
 #%%[markdown]
-# Standardise the country names.
+# Standardise the country names and drop rows with NA values.
 # %%
 stand_countries = pd.read_csv(
     "data/input/countries_to_standardise_country_standardized.csv"
@@ -223,6 +244,7 @@ df_gap = df_gap.dropna()
 # ## Reading in the WHO data
 
 # Read in data from WDI - in future iterations we will read this in from ETL so it is auto updated. The WDI sources this variable from the WHO so we will henceforth refer to it as the WHO data.
+# This data covers from the year 2000 onwards. A previous version had data from the year 1990 onwards, we will also read this in and use this preferentially over the GapMinder data.
 
 # %%
 r_who = requests.get(
@@ -239,7 +261,8 @@ who = pd.read_csv(
 
 #%%[markdown]
 ## Cleaning the WHO data
-# Pivot and clean the WHO data and standardise the country names.
+# Pivot and clean the WHO data and standardise the country names and drop values with NA values.
+
 # %%
 year_cols = list(range(1960, 2022))
 year_cols = [str(int) for int in year_cols]
@@ -256,50 +279,89 @@ who_df.rename(
 who_df["source"] = "who"
 who_df["entity"] = who_df["entity"].map(stan_dict)
 who_df = who_df.dropna()
-
 #%%[markdown]
 # ## Merging the WHO data with the GapMinder data
 
 # Combine the two datasets. For years where there is data from both datasets we preferentially keep data from the WHO.
 
+# %% [markdown]
+# Add in the OECD Stat Health data
+
 # %%
-df = pd.concat([df_gap, who_df], ignore_index=True)
-df["year"] = df["year"].astype(int)
+oecd_df = pd.read_csv("data/input/HEALTH_STAT_21072022104153993.csv")
+oecd_df = oecd_df[["Country", "Year", "Value"]].rename(
+    columns={"Country": "entity", "Year": "year", "Value": "maternal_mortality_rate"}
+)
+oecd_df["source"] = "oecd"
+
+oecd_df["entity"] = oecd_df["entity"].map(stan_dict)
 
 
 # %% [markdown]
-# Get the first years data from WHO and the last years data from GapMinder - to compare the values when the source changes. 
+# Read in the older WHO data taken from a previous version of the WDI - we just want the data for 1990-1999
 
 # %%
-df_gap.sort_values(by = ['entity', 'year'], inplace=True)
-df_gap_gr = df_gap.groupby('entity')
-who_df.sort_values(by = ['entity', 'year'], inplace=True)
-who_df_gr = who_df.groupby('entity')
+who_2015 = pd.read_csv("data/input/maternal-mortality_wb_2015.csv")
+who_2015["source"] = "who"
+who_2015 = who_2015[
+    [
+        "Entity",
+        "Year",
+        "Maternal Mortality Ratio (Gapminder (2010) and World Bank (2015))",
+        "source",
+    ]
+].rename(
+    columns={
+        "Entity": "entity",
+        "Year": "year",
+        "Maternal Mortality Ratio (Gapminder (2010) and World Bank (2015))": "maternal_mortality_rate",
+    }
+)
+who_2015 = who_2015[(who_2015["year"] < 2000) & (who_2015["year"] >= 1990)]
 
-first_last_df = (pd.concat([who_df_gr.head(1), df_gap_gr.tail(1)])
-   .drop_duplicates()
-   .reset_index(drop=True)
-   .dropna())
+# %%
+df = pd.concat([df_gap, who_2015, who_df, oecd_df], ignore_index=True)
+df["year"] = df["year"].astype(int)
+df
+
+
+# %% [markdown]
+# Get the first years data from WHO and the last years data from GapMinder - to compare the values when the source changes.
+#
+
+# %%
+df_gap.sort_values(by=["entity", "year"], inplace=True)
+df_gap_gr = df_gap.groupby("entity")
+who_df.sort_values(by=["entity", "year"], inplace=True)
+who_df_gr = who_df.groupby("entity")
+
+first_last_df = (
+    pd.concat([who_df_gr.head(1), df_gap_gr.tail(1)])
+    .drop_duplicates()
+    .reset_index(drop=True)
+    .dropna()
+)
 
 # %% [markdown]
 # We want to look at countries which feature in both datasets.
+#
 
 # %%
-df_c= first_last_df.entity.value_counts()
-first_last_df = first_last_df[first_last_df.entity.isin(df_c.index[df_c.gt(1)])].sort_values(by = ['entity', 'year'],ascending = [True, False])
+df_c = first_last_df.entity.value_counts()
+first_last_df = first_last_df[
+    first_last_df.entity.isin(df_c.index[df_c.gt(1)])
+].sort_values(by=["entity", "year"], ascending=[True, False])
+
+# %%
+df_dup = df[(df.entity.isin(first_last_df.entity)) & (df.year >= 1970)]
+
 
 # %% [markdown]
-# WHO values are consistently higher than the GapMinder values when you compare the trends for countries with overlapping or near overlapping time-series
+# Plotting time-series of the data from 1985 onwards. GapMinder is shown in orange, WHO is shown in blue and OECD n green.
+#
 
 # %%
-df_dup = df[(df.entity.isin(first_last_df.entity)) & (df.year >= 1985)]
-
-
-# %% [markdown]
-# Plotting time-series of the data from 1985 onwards. GapMinder is shown in orange and WHO is shown in blue.
-
-# %%
-plt.figure(figsize=(10,8),facecolor='white')
+plt.figure(figsize=(10, 8), facecolor="white")
 
 # plot numbering starts at 1, not 0
 plot_number = 1
@@ -309,19 +371,40 @@ for countryname, selection in df_dup.groupby("entity"):
     ax = plt.subplot(5, 3, plot_number)
     selection_gap = selection[selection["source"] == "gapminder"]
     selection_who = selection[selection["source"] == "who"]
-    selection_who.plot(x='year', y='maternal_mortality_rate', ax=ax, label=countryname, legend=False)
-    selection_gap.plot(x='year', y='maternal_mortality_rate', ax=ax, label=countryname, legend=False)
+    selection_oecd = selection[selection["source"] == "oecd"]
+    if selection_who.shape[0] > 0:
+        selection_who.plot(
+            x="year",
+            y="maternal_mortality_rate",
+            ax=ax,
+            label=countryname,
+            legend=False,
+        )
+    if selection_gap.shape[0] > 0:
+        selection_gap.plot(
+            x="year",
+            y="maternal_mortality_rate",
+            ax=ax,
+            label=countryname,
+            legend=False,
+        )
+    if selection_oecd.shape[0] > 0:
+        selection_oecd.plot(
+            x="year",
+            y="maternal_mortality_rate",
+            ax=ax,
+            label=countryname,
+            legend=False,
+        )
     ax.set_title(countryname)
     # Go to the next plot for the next loop
     plot_number = plot_number + 1
 plt.tight_layout()
 
 # %%
-
 #%%[markdown]
 # ## Cleaning geographic entities
 # We don't want to include all of the available regions in the data as it is difficult for us to define them clearly, so we drop out a selection here.
-# %%
 entities_to_drop = [
     "Africa Eastern and Southern",
     "Africa Western and Central",
