c0ce37e25ba92eab43d1007f3abacc3c4a5c745d
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-07-21 21:13:42 +0300

    Compare PIP and WID

---
+++ BetterDataDocs/JoeHasell/PhD_global_dist/compare_wid_pip_gini_and_topshares.r
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+# Pull in PIP data
+
+df_pip<- read.csv("https://joeh.fra1.digitaloceanspaces.com/wid/gini_and_topshares_standardized.csv")
+
+
+
+df_wid<- read.csv("https://joeh.fra1.digitaloceanspaces.com/wid/gini_and_topshares_standardized.csv")
