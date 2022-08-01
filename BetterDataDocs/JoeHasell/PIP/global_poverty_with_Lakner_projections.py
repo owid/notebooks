4ff6553002f1bdf59842aadcf10f7853549c5e47
parriagadap <paarriagadap@gmail.com>
2022-08-01 12:15:15 -0400

    Additional formatting

--- BetterDataDocs/JoeHasell/PIP/global_poverty_with_Lakner_projections.py
+++ BetterDataDocs/JoeHasell/PIP/global_poverty_with_Lakner_projections.py
@@ -97,7 +97,11 @@ fig.show()
 fig.write_image("graphics/global_pov_with_projections.svg")
 
 # %% [markdown]
-# ## 
+# ## Second chart
+#
+# It comes from this [2020 article](https://blogs.worldbank.org/opendata/projecting-global-extreme-poverty-2030-how-close-are-we-world-banks-3-goal), estimating the population in poverty by region and the projection to 2030.
+#
+# One option to replicate it is to get the (non-projected) data from PIP's API and manually copy the projections from the graph. The oldest version available is more recent than the article, `20210401_2011_02_02_PROD`
 
 # %%
 povline='1.9'
@@ -110,12 +114,21 @@ df_stacked = df_stacked[(df_stacked['region_name'] != "World") &
                        (df_stacked['reporting_year'] >= 1990)].reset_index(drop=True)
 
 
+# %% [markdown]
+# The data from the API does not have the same values. For instance, in 1990 the number of poor in East Asia is 977.17 million people (vs 976.87) and in Latin America and the Caribbean is 69.62 million (vs. 66.3). So this option is discarded.
+
 # %%
 df_stacked[(df_stacked['reporting_year'] == 1990)]
+
+# %% [markdown]
+# These data also shows missing values for South Asia between 1997 and 2001, so it would also need to be transformed
 
 # %%
 fig = px.area(df_stacked, x="reporting_year", y="pop_in_poverty", color="region_name")
 fig.show()
+
+# %% [markdown]
+# As the graph is visualised with Flourish, the underlying data is available internally as json, so it can be extracted.
 
 # %%
 #Based on https://stackoverflow.com/questions/62031809/extracting-javascript-variables-into-python-dictionaries
@@ -137,6 +150,9 @@ print(json.dumps(data, indent=4))
 
 #for row in data['data']:
 #    print('{:<55}{}'.format(*map(str.strip, row['filter'][:2])))
+
+# %% [markdown]
+# The values are show as lists in each row, so further transformations are required.
 
 # %%
 df_flourish = pd.DataFrame.from_dict(data['data'])
@@ -200,7 +216,11 @@ df_stacked
 estimation_list = list(df_stacked['c19_estimation'].unique())
 
 for i in estimation_list:
-    fig = px.area(df_stacked[df_stacked['c19_estimation'] == i], x="year", y="pop_poverty", color="region", title=i)
+    fig = px.area(df_stacked[df_stacked['c19_estimation'] == i], x="year", y="pop_poverty", color="region",
+                  title=f'Estimation: <b>{i}</b>', template='none')
+    fig.add_vrect(x0=2018, x1=2030, line_width=0, fillcolor="#8b9598", opacity=0.2,
+                 annotation_text="<b>Projection</b>", annotation_position="top left")
+
     fig.write_image(f'graphics/global_pov_with_projections_region_{i}.svg')
     fig.show()
 
