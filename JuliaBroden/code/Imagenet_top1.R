4f9be748b2d5695a04361919f6e30e53880ab224
Edouard Mathieu <edmat@pm.me>
2022-08-05 11:02:45 +0200

    Update Imagenet_top1.R
    - Comment out unused code
    - Set sheet name (in case they change sheet order)
    - Unquote column name in select()
    - Remove vector notation from rename()
    - Write file to subfolder

--- JuliaBroden/code/Imagenet_top1.R
+++ JuliaBroden/code/Imagenet_top1.R
@@ -8,23 +8,23 @@ sheet_url <- "https://docs.google.com/spreadsheets/d/1C1aJ9TVVCYDvRHsf3i8fhFCm7U
 
 ### Version 1: With or without extra training data as entity
 
-df <- read_sheet(sheet_url)
-
-df <- df %>% 
-  rename(c(Entity = `Type`, Top5_accuracy = `Top-5 Accuracy`)) %>%
-  mutate(Entity = str_to_sentence(Entity)) %>%
-  relocate(Entity, Year, Top5_accuracy, Method)
-
-write_csv(df, "Imagenet_top1_V1.csv")
+# df <- read_sheet(sheet_url)
+# 
+# df <- df %>% 
+#   rename(c(Entity = `Type`, Top5_accuracy = `Top-5 Accuracy`)) %>%
+#   mutate(Entity = str_to_sentence(Entity)) %>%
+#   relocate(Entity, Year, Top5_accuracy, Method)
+# 
+# write_csv(df, "Imagenet_top1_V1.csv")
 
 ### Version 2: Method as entity
 
-df2 <- read_sheet(sheet_url)
+df2 <- read_sheet(sheet_url, sheet = "ImageNet: Top-1 Accuracy")
 
 df2 <- df2 %>% 
-  select(-'Source') %>%
-  rename(c(Entity = `Method`, Top1_accuracy = `Top-1 Accuracy`, Imagenet_extra_training_data = `Type`)) %>%
+  select(-Source) %>%
+  rename(Entity = Method, Top1_accuracy = `Top-1 Accuracy`, Imagenet_extra_training_data = Type) %>%
   mutate(Entity = str_to_sentence(Entity)) %>%
   relocate(Entity, Year, Top1_accuracy, Imagenet_extra_training_data)
 
-write_csv(df2, "Imagenet_top1_V2.csv")
+write_csv(df2, "transformed/Imagenet_top1_V2.csv")
