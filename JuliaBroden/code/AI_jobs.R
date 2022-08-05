b68cc26f0bcdaa2071641204b335b5384e18acd9
Edouard Mathieu <edmat@pm.me>
2022-08-05 10:52:14 +0200

    Update AI_jobs.R
    - Import necessary package
    - Write file to subfolder

--- JuliaBroden/code/AI_jobs.R
+++ JuliaBroden/code/AI_jobs.R
@@ -3,6 +3,7 @@ library(stringr)
 library(googlesheets4)
 library(dplyr)
 library(readr)
+library(tidyr)
 
 # Fig 4.1.3 share of ai jobs among all job postings
 sheet_url <- "https://docs.google.com/spreadsheets/d/19UGjvQW-PcnWoHEPe9Wd1Eib5z6S5II7lu1tE92fK3M/edit#gid=114714324"
@@ -14,4 +15,4 @@ df <- df %>%
   relocate(Entity, Year)
 
 # percent or decimal?
-write_csv(df, "AI_jobs.csv")
+write_csv(df, "transformed/AI_jobs.csv")
