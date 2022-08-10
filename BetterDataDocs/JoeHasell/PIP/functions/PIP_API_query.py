108f4f3fad511bf61396362f3a21014481837b29
parriagadap <paarriagadap@gmail.com>
2022-08-10 19:16:06 -0400

    Request query to avoid WinError 10060

--- BetterDataDocs/JoeHasell/PIP/functions/PIP_API_query.py
+++ BetterDataDocs/JoeHasell/PIP/functions/PIP_API_query.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import requests
+import io
 
 def pip_query_country(popshare_or_povline, value, country_code="all", year="all", fill_gaps="true", welfare_type="all", reporting_level="all"):
 
     # Build query
     request_url = f'https://api.worldbank.org/pip/v1/pip?{popshare_or_povline}={value}&country={country_code}&year={year}&fill_gaps={fill_gaps}&welfare_type={welfare_type}&reporting_level={reporting_level}&format=csv'
 
-    df = pd.read_csv(request_url)
+    #df = pd.read_csv(request_url)
+    response = requests.get(request_url, timeout=20).content
+    df = pd.read_csv(io.StringIO(response.decode('utf-8')))
 
     return df
 
@@ -16,6 +20,8 @@ def pip_query_region(povline, year="all"):
     # Build query
     request_url = f'https://api.worldbank.org/pip/v1/pip-grp?povline={povline}&year={year}&group_by=wb&format=csv'
 
-    df = pd.read_csv(request_url)
+    #df = pd.read_csv(request_url)
+    response = requests.get(request_url, timeout=20).content
+    df = pd.read_csv(io.StringIO(response.decode('utf-8')))
 
     return df
