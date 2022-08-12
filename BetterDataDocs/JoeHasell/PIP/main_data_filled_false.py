a96c3fa55099044980c782ac20389373b6518a73
parriagadap <paarriagadap@gmail.com>
2022-08-11 20:33:34 -0400

    Patching missing media data

--- BetterDataDocs/JoeHasell/PIP/main_data_filled_false.py
+++ BetterDataDocs/JoeHasell/PIP/main_data_filled_false.py
@@ -138,6 +138,60 @@ print('Execution time:', elapsed_time, 'seconds')
 
 
 # %% [markdown]
+# ## Patching missing median data
+
+# %%
+start_time = time.time()
+
+df_query = pip_query_country(
+                    popshare_or_povline = "povline", 
+                    value = 1.9, 
+                    fill_gaps="false")
+
+median_list = []
+
+for i in range(len(df_query)):
+    if np.isnan(df_query['median'][i]):
+        df_popshare = pip_query_country(popshare_or_povline = "popshare",
+                                        country_code = df_query['country_code'][i],
+                                        year = df_query['reporting_year'][i],
+                                        welfare_type = df_query['welfare_type'][i],
+                                        reporting_level = df_query['reporting_level'][i],
+                                        value = 0.5,
+                                        fill_gaps="false")
+        try:
+            median_value = df_popshare['poverty_line'][0]
+            median_list.append(median_value)
+        
+        except:
+            median_list.append(np.nan)
+        
+        
+    else:
+        median_value = df_query['median'][i]
+        median_list.append(median_value)
+               
+
+df_query['median2'] = median_list
+null_median = (df_query['median'].isnull()).sum()
+null_median2 = (df_query['median2'].isnull()).sum()
+print(f'Before patching: {null_median} nulls for median')
+print(f'After patching: {null_median2} nulls for median')
+
+df_complete = pd.merge(df_complete, df_query[['country_name', 'reporting_year', 'welfare_type', 'reporting_level', 'median2']], 
+                       how='left', 
+                       left_on=['Entity', 'reporting_year', 'welfare_type', 'reporting_level'], 
+                       right_on=['country_name', 'reporting_year', 'welfare_type', 'reporting_level'], 
+                       validate='many_to_one')
+df_complete.drop(columns=['median'], inplace=True)
+df_complete.rename(columns={'median2': 'median'}, inplace=True)
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Execution time:', elapsed_time, 'seconds')
+
+# %% [markdown]
 # ## Data transformations
 
 # %%
