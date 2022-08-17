b0c5491be68c4e286f83c66a68dda58868541854
parriagadap <paarriagadap@gmail.com>
2022-08-16 23:27:15 -0400

    Increase timeout to 100

--- BetterDataDocs/JoeHasell/PIP/functions/PIP_API_query.py
+++ BetterDataDocs/JoeHasell/PIP/functions/PIP_API_query.py
@@ -8,7 +8,7 @@ def pip_query_country(popshare_or_povline, value, country_code="all", year="all"
     request_url = f'https://api.worldbank.org/pip/v1/pip?{popshare_or_povline}={value}&country={country_code}&year={year}&fill_gaps={fill_gaps}&welfare_type={welfare_type}&reporting_level={reporting_level}&format=csv'
 
     #df = pd.read_csv(request_url)
-    response = requests.get(request_url, timeout=50).content
+    response = requests.get(request_url, timeout=100).content
     df = pd.read_csv(io.StringIO(response.decode('utf-8')))
 
     return df
@@ -21,7 +21,7 @@ def pip_query_region(povline, year="all"):
     request_url = f'https://api.worldbank.org/pip/v1/pip-grp?povline={povline}&year={year}&group_by=wb&format=csv'
 
     #df = pd.read_csv(request_url)
-    response = requests.get(request_url, timeout=50).content
+    response = requests.get(request_url, timeout=100).content
     df = pd.read_csv(io.StringIO(response.decode('utf-8')))
 
     return df
