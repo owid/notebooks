3ef00dc85bebc9e5a396f1615971c96668c09708
spoonerf <spoonerfiona@gmail.com>
2022-08-18 14:30:55 +0100

    feat(flu): code and cleaned flunet data output

--- FionaSpooner/flunet/flu_utils.py
+++ FionaSpooner/flunet/flu_utils.py
@@ -26,3 +26,69 @@ def download_country_flu_data(
     for country_code in country_codes:
         data_df = get_flu_data(base_url, country_code)
         data_df.to_csv(f"{data_dir}{country_code}.csv", index=False, escapechar="\\")
+
+
+def combine_country_datasets(data_dir: str, country_codes: list) -> pd.DataFrame:
+    data_dfs = [
+        pd.read_csv(f"{data_dir}{country_code}.csv") for country_code in country_codes
+    ]
+    combined_df = pd.concat(data_dfs)
+    return combined_df
+
+
+def aggregate_surveillance_type(
+    combined_df: pd.DataFrame, sel_cols: list
+) -> pd.DataFrame:
+
+    df = combined_df[sel_cols]
+    df = df.copy(deep=True)
+    df["date"] = pd.to_datetime(
+        df["ISO_WEEKSTARTDATE"], format="%Y-%m-%d", utc=True
+    ).dt.date
+    df_agg = (
+        df.groupby(["COUNTRY_CODE", "COUNTRY/AREA/TERRITORY", "ISO_YEAR", "date"])
+        .sum()
+        .reset_index()
+    )
+    # Check we haven't lost any cases along the way
+    assert combined_df["INF_ALL"].sum() == df_agg["INF_ALL"].sum()
+    return df_agg
+
+
+def combine_columns_calc_percent(df: pd.DataFrame) -> pd.DataFrame:
+    df["ANOTSUBTYPED"] = (
+        df["ANOTSUBTYPED"] + df["ANOTSUBTYPABLE"] + df["AOTHER_SUBTYPE"]
+    )
+    df["BVIC"] = (
+        df["BVIC_2DEL"] + df["BVIC_3DEL"] + df["BVIC_NODEL"] + df["BVIC_DELUNK"]
+    )
+    df["pcnt_pos"] = (df["INF_ALL"] / (df["INF_ALL"] + df["INF_NEGATIVE"])) * 100
+    df["pcnt_pos"] = df["pcnt_pos"].fillna(0).round(2)
+    df["year"] = df["ISO_YEAR"].astype(int)
+    assert (df["pcnt_pos"] <= 100).all()
+    df = df.drop(
+        [
+            "ANOTSUBTYPABLE",
+            "AOTHER_SUBTYPE",
+            "BVIC_2DEL",
+            "BVIC_3DEL",
+            "BVIC_NODEL",
+            "BVIC_DELUNK",
+            "ISO_YEAR",
+            "COUNTRY_CODE",
+        ],
+        axis=1,
+    )
+    df.rename(columns={"COUNTRY/AREA/TERRITORY": "Country"}, inplace=True)
+
+    return df
+
+
+def standardise_countries(df: pd.DataFrame) -> pd.DataFrame:
+    stan_countries = pd.read_csv(
+        "FionaSpooner/flunet/country_codes_country_standardized.csv"
+    )
+    stan_dict = stan_countries.set_index("Country").squeeze().to_dict()
+    df["Country"] = df["Country"].map(stan_dict)
+
+    return df
--- FionaSpooner/flunet/flunet_fluid_data.py
+++ FionaSpooner/flunet/flunet_fluid_data.py
@@ -1,10 +1,44 @@
 import os
 from pathlib import Path
-from flu_utils import get_country_codes, download_country_flu_data
+from .flu_utils import aggregate_surveillance_type, standardise_countries
+from flu_utils import (
+    get_country_codes,
+    download_country_flu_data,
+    combine_country_datasets,
+    aggregate_surveillance_type,
+    combine_columns_calc_percent,
+)
 
 path = str(Path(__file__).parent.resolve())
+path = "/Users/fionaspooner/Documents/OWID/repos/notebooks/FionaSpooner/flunet/"
 flunet_data_dir = os.path.join(path, "data/flunet/")
 flunet_url = "https://frontdoor-l4uikgap6gz3m.azurefd.net/FLUMART/VIW_FNT"
+flunet_cols = [
+    "COUNTRY/AREA/TERRITORY",
+    "COUNTRY_CODE",
+    "ISO_WEEKSTARTDATE",
+    "ISO_YEAR",
+    "ORIGIN_SOURCE",
+    "AH1N12009",
+    "AH1",
+    "AH3",
+    "AH5",
+    "AH7N9",
+    "ANOTSUBTYPED",
+    "ANOTSUBTYPABLE",
+    "AOTHER_SUBTYPE",
+    "INF_A",
+    "BVIC_2DEL",
+    "BVIC_3DEL",
+    "BVIC_NODEL",
+    "BVIC_DELUNK",
+    "BYAM",
+    "BNOTDETERMINED",
+    "INF_B",
+    "INF_ALL",
+    "INF_NEGATIVE",
+]
+
 fluid_data_dir = os.path.join(path, "data/fluid/")
 fluid_url = "https://frontdoor-l4uikgap6gz3m.azurefd.net/FLUMART/VIW_FID"
 
@@ -15,6 +49,13 @@ def main():
     download_country_flu_data(
         data_dir=flunet_data_dir, base_url=flunet_url, country_codes=country_codes
     )
+    combined_df = combine_country_datasets(
+        data_dir=flunet_data_dir, country_codes=country_codes
+    )
+    df = aggregate_surveillance_type(combined_df, flunet_cols)
+    df = combine_columns_calc_percent(df)
+    flunet_df = standardise_countries(df)
+    flunet_df.to_csv(os.path.join(path, "data", "flunet.csv"), index=False)
     # Get country codes and download Fluid data for each country
     country_codes = get_country_codes(base_url=fluid_url)
     download_country_flu_data(
