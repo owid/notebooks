5c27f226ca4f1fb63357e8325ddde1f6e89d2337
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-09-01 15:48:58 +0100

    Restructure for loop on vars

--- BetterDataDocs/JoeHasell/PIP/old/test_show_breaks_dataset.py
+++ BetterDataDocs/JoeHasell/PIP/old/test_show_breaks_dataset.py
@@ -18,19 +18,23 @@ df = df_orig
 df = df[df['survey_comparability'].notna()]
 
 #%%
+# FORMAT COMPARABILTY VAR
+# Add 1 to make comparability var run from 1, not from 0
+df['survey_comparability'] = df['survey_comparability'] + 1
+
+# Make a more friendly slug for when the data gets pivoted wider
 df['survey_comparability'] = 'comparable_spell_' + df['survey_comparability'].astype(int).astype(str)
 
 # %%
-select_var = 'headcount_ratio_190'
 
-df = df[['Entity', 'Year', select_var, 'survey_comparability']]
-# %%
-# convert t0 wide
-df = pd.pivot(df, index=['Entity', 'Year'], columns=['survey_comparability'], values=select_var).reset_index()
+for select_var in ['headcount_ratio_190']:
 
+    df_var = df[['Entity', 'Year', select_var, 'survey_comparability']]
 
+    # convert t0 wide
+    df_var = pd.pivot(df_var, index=['Entity', 'Year'], columns=['survey_comparability'], values=select_var).reset_index()
 
-# %%
-# write to csv
-df.to_csv("test_of_show_breaks.csv", index = False)
+    
+    # write to csv – one csv per variable in the main dataset
+    df_var.to_csv(f'comparability_data/{select_var}.csv', index = False)
 # %%
