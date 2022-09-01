7ea66a81794eecc9c3a1726b1348fd86e63e0a0e
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-09-01 16:08:45 +0100

    Run comparabilty split for all vars

--- BetterDataDocs/JoeHasell/PIP/old/test_show_breaks_dataset.py
+++ BetterDataDocs/JoeHasell/PIP/old/test_show_breaks_dataset.py
@@ -9,25 +9,27 @@ fp = '/Users/joehasell/Documents/OWID/notebooks/BetterDataDocs/JoeHasell/PIP/dat
 df_orig = pd.read_csv(fp)
 
 df = df_orig
-#%%
-#select_country = 'Kenya'
-#df = df_orig[df_orig['Entity']== select_country]
+
+vars = [i for i in df.columns if i not in ["Entity", "Year", "reporting_pop"]]
+
 
 # %%
-# drop regions where survey coverage = nan
+# drop rows where survey coverage = nan (This is just regions)
 df = df[df['survey_comparability'].notna()]
+
 
 #%%
 # FORMAT COMPARABILTY VAR
+
 # Add 1 to make comparability var run from 1, not from 0
 df['survey_comparability'] = df['survey_comparability'] + 1
 
-# Make a more friendly slug for when the data gets pivoted wider
-df['survey_comparability'] = 'comparable_spell_' + df['survey_comparability'].astype(int).astype(str)
+# Note the welfare type in the comparability spell 
+df['survey_comparability'] = df['welfare_type'] + '_spell_' + df['survey_comparability'].astype(int).astype(str)
 
 # %%
 
-for select_var in ['headcount_ratio_190']:
+for select_var in ['headcount_ratio_100', 'headcount_ratio_190']:
 
     df_var = df[['Entity', 'Year', select_var, 'survey_comparability']]
 
