f84d469420702c13179142c78cac930eb5ae1a01
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-09-01 15:04:05 +0100

    Add csv to test showing breaks

---
+++ BetterDataDocs/JoeHasell/PIP/old/test_show_breaks_dataset.py
@@ -0,0 +1,36 @@
+
+#%%
+import pandas as pd
+
+
+
+#%%
+fp = '/Users/joehasell/Documents/OWID/notebooks/BetterDataDocs/JoeHasell/PIP/data/poverty_inc_or_cons.csv'
+df_orig = pd.read_csv(fp)
+
+df = df_orig
+#%%
+#select_country = 'Kenya'
+#df = df_orig[df_orig['Entity']== select_country]
+
+# %%
+# drop regions where survey coverage = nan
+df = df[df['survey_comparability'].notna()]
+
+#%%
+df['survey_comparability'] = 'survey_group_' + df['survey_comparability'].astype(int).astype(str)
+
+# %%
+select_var = 'headcount_ratio_190'
+
+df = df[['Entity', 'Year', select_var, 'survey_comparability']]
+# %%
+# convert t0 wide
+df = pd.pivot(df, index=['Entity', 'Year'], columns=['survey_comparability'], values=select_var).reset_index()
+
+
+
+# %%
+# write to csv
+df.to_csv("test_of_show_breaks.csv", index = False)
+# %%
