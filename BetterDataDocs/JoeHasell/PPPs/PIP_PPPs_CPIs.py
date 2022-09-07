877695c424bccaeaa9a24d65c8883321db96ef13
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-09-07 07:15:04 +0100

    Create PIP_PPPs_CPIs.py

---
+++ BetterDataDocs/JoeHasell/PPPs/PIP_PPPs_CPIs.py
@@ -0,0 +1,59 @@
+
+#%%
+import pandas as pd
+import requests
+import io
+import wbgapi as wb
+
+
+#%%
+def pip_query_country(popshare_or_povline, value, country_code="all", year="all", fill_gaps="true", welfare_type="all", reporting_level="all"):
+
+    # Build query
+    request_url = f'https://api.worldbank.org/pip/v1/pip?{popshare_or_povline}={value}&country={country_code}&year={year}&fill_gaps={fill_gaps}&welfare_type={welfare_type}&reporting_level={reporting_level}&format=csv'
+
+    #df = pd.read_csv(request_url)
+    response = requests.get(request_url, timeout=200).content
+    df = pd.read_csv(io.StringIO(response.decode('utf-8')))
+
+    return df
+
+#%%
+
+df_PIP = pip_query_country("povline", 1.90,fill_gaps="false" )
+
+#%%
+
+
+df_WDI = wb.data.DataFrame({'PA.NUS.PRVT.PP': "PPP_PrivCons_WDI","FP.CPI.TOTL":"CPI_WDI"}, "all", time=range(2011, 2020)).reset_index()
+
+# Pivot longer
+# %%
+# compare WDI and PIP and CPIs
+
+
+# %%
+# Check calculation of annual PPP series – 2017 PPPs updated with ratio of country and US CPI.
+
+
+
+
+# %%
+# Pull in global income distribution
+
+
+# %%
+# Add input for your household income and size of household and local currency (just your country)
+
+
+
+# %%
+# Calculate rough position in global economy. (ideally – where are you in your own country, and where are you globally)
+
+
+# %%
+# Link to country specific searches
+
+
+
+
