29258bb4b12f3edc6a57a67d40a837053ed23dc1
parriagadap <paarriagadap@gmail.com>
2022-09-29 14:35:57 +0100

    Added version

--- BetterDataDocs/JoeHasell/PIP/functions/PIP_API_query.py
+++ BetterDataDocs/JoeHasell/PIP/functions/PIP_API_query.py
@@ -4,8 +4,14 @@ import io
 
 def pip_query_country(popshare_or_povline, value, country_code="all", year="all", fill_gaps="true", welfare_type="all", reporting_level="all", ppp_version=2011):
 
+    if ppp_version == 2011:
+        version = "20220909_2011_02_02_PROD"
+        
+    elif ppp_version == 2017:
+        version = "20220909_2017_01_02_PROD"
+        
     # Build query
-    request_url = f'https://api.worldbank.org/pip/v1/pip?{popshare_or_povline}={value}&country={country_code}&year={year}&fill_gaps={fill_gaps}&welfare_type={welfare_type}&reporting_level={reporting_level}&ppp_version={ppp_version}&format=csv'
+    request_url = f'https://api.worldbank.org/pip/v1/pip?{popshare_or_povline}={value}&country={country_code}&year={year}&fill_gaps={fill_gaps}&welfare_type={welfare_type}&reporting_level={reporting_level}&ppp_version={ppp_version}&version={version}&format=csv'
     status = 0
     
     while status != 200:
@@ -22,8 +28,15 @@ def pip_query_country(popshare_or_povline, value, country_code="all", year="all"
 # For world regions, the popshare query is not available (or rather, it returns nonsense).
 def pip_query_region(povline, year="all", ppp_version=2011):
 
+    if ppp_version == 2011:
+        version = "20220909_2011_02_02_PROD"
+        
+    elif ppp_version == 2017:
+        version = "20220909_2017_01_02_PROD"
+    
+
     # Build query
-    request_url = f'https://api.worldbank.org/pip/v1/pip-grp?country=all&povline={povline}&year={year}&ppp_version={ppp_version}&group_by=wb&format=csv'
+    request_url = f'https://api.worldbank.org/pip/v1/pip-grp?country=all&povline={povline}&year={year}&ppp_version={ppp_version}&version={version}&group_by=wb&format=csv'
     status = 0
     
     while status != 200:
