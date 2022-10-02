ae2b9e443584b40ff2e83c08d0273ffe6872d37d
JoeHasell <41148640+JoeHasell@users.noreply.github.com>
2022-10-02 16:59:58 +0100

    Update test_2017_PPPs.py

--- BetterDataDocs/JoeHasell/random/test_2017_PPPs.py
+++ BetterDataDocs/JoeHasell/random/test_2017_PPPs.py
@@ -2,6 +2,9 @@
 from heapq import merge
 import pandas as pd
 from plotnine import *
+import requests
+import io
+
 
 #%%
 fp = 'https://raw.githubusercontent.com/owid/notebooks/main/BetterDataDocs/JoeHasell/PIP/data/ppp_2011/final/OWID_internal_upload/admin_database/pip_final.csv'
@@ -114,10 +117,15 @@ request_url = 'https://api.worldbank.org/pip/v1/pip?' + \
 
 #%%
 country_code = 'ETH'
-year = 2016
 povline = 10
+year = 2015
  # Build query
-request_url = 'https://api.worldbank.org/pip/v1/pip?' + \
+request_url = f'https://api.worldbank.org/pip/v1/pip?povline={povline}&country={country_code}&year={year}&fill_gaps=false&welfare_type=all&reporting_level=national&version=20220909_2017_01_02_PROD&format=csv'
+
+df5 =  pd.read_csv(request_url)
+#%%
+
+'https://api.worldbank.org/pip/v1/pip?' + \
       'country=' + country_code + \
       '&year=' + 'all' + \
       '&povline=' + str(povline) + \
@@ -127,7 +135,43 @@ request_url = 'https://api.worldbank.org/pip/v1/pip?' + \
       '&reporting_level=national' + \
       '&format=csv'
 
-df =  pd.read_csv(request_url)
+df4 =  pd.read_csv(request_url)
 
 
 # %%
+
+# Build query
+request_url = f'https://api.worldbank.org/pip/v1/pip?povline=10&country=ETH&year=all&fill_gaps=false&welfare_type=all&reporting_level=national&version=20220909_2017_01_02_PROD&format=csv'
+df3 =  pd.read_csv(request_url)
+
+# %%
+status = 0
+
+response = requests.get(request_url, timeout=500)
+content = response.content
+status = response.status_code
+    
+df = pd.read_csv(io.StringIO(content.decode('utf-8')))
+
+# %%
+
+
+# For world regions, the popshare query is not available (or rather, it returns nonsense).
+def pip_query_region(povline, year="all", ppp_version=2011):
+
+    if ppp_version == 2011:
+        version = "20220909_2011_02_02_PROD"
+        
+    elif ppp_version == 2017:
+        version = "20220909_2017_01_02_PROD"
+    
+
+    # Build query
+    request_url = f'https://api.worldbank.org/pip/v1/pip-grp?country=all&povline={povline}&year={year}&ppp_version={ppp_version}&version={version}&group_by=wb&format=csv'
+    status = 0
+    
+    while status != 200:
+        #df = pd.read_csv(request_url)
+        response = requests.get(request_url, timeout=500)
+        content = response.content
+        status = response.status_code
