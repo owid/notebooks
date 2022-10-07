04e6d81c315493795a3d3c7290347d5f0c1dee14
parriagadap <paarriagadap@gmail.com>
2022-10-07 09:56:21 +0100

    2017 PPPs done

--- BetterDataDocs/JoeHasell/PIP/ppp_comparison_explorer.py
+++ BetterDataDocs/JoeHasell/PIP/ppp_comparison_explorer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # +
 #Read Google sheets
@@ -29,6 +30,9 @@ url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sh
 survey_type = pd.read_csv(url)
 # -
 
+dict_table = table_base.to_dict('records')
+dict_table
+
 survey_type
 
 povlines_ppp2011
@@ -42,11 +46,27 @@ for i in range(len(table_base)):
         for p_2011 in range(len(povlines_ppp2011)):
             for p in range(len(povlines_both)):
                 for survey in range(len(survey_type)):
-                    hola = table_base['name'][i]
-                    print('{}'.format(table_base['name'][i]))
-                    #df['name'][j] = table_base['name'][i]
+                    df.loc[j, 'name'] = f'Below ${povlines_ppp2011.dollars[p_2011]} a day (2011 prices)'
+                    df.loc[j, 'slug'] = f'headcount_ratio_{povlines_ppp2011.cents[p_2011]}_ppp2011'
+                    df.loc[j, 'sourceName'] = "World Bank Poverty and Inequality Platform"
+                    df.loc[j, 'description'] = f'% of population living in households with an income or expenditure per person below ${povlines_ppp2011.dollars[p_2011]} a day (2011 prices).'
+                    df.loc[j, 'sourceLink'] = "https://pip.worldbank.org/"
+                    df.loc[j, 'dataPublishedBy'] = "World Bank Poverty and Inequality Platform (PIP)"
+                    df.loc[j, 'unit'] = "%"
+                    df.loc[j, 'shortUnit'] = "%"
+                    df.loc[j, 'tolerance'] = 5
+                    df.loc[j, 'retrievedDate'] = np.nan
+                    df.loc[j, 'type'] = "Numeric"
+                    df.loc[j, 'colorScaleNumericMinValue'] = 0
+                    df.loc[j, 'colorScaleNumericBins'] = "3;10;20;30;40;50;60;70;80;90;100"
+                    df.loc[j, 'colorScaleEqualSizeBins'] = "true"
+                    df.loc[j, 'colorScaleScheme'] = "OrRd"
                     j += 1
 # -
+
+df
+
+table_base.loc[0, 'name']
 
 print('Below ${hola} a day (2017 prices)'.format(hola= povlines_ppp2017['dollars'][1]))
 
