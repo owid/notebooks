843ef7bb856b5db3af0fe2c6307014833e93c1be
Julia Broden <58789198+jsbroden@users.noreply.github.com>
2022-11-22 17:11:40 +0100

    Add files via upload

--- JuliaBroden/code/Private_investment_ai.R
+++ JuliaBroden/code/Private_investment_ai.R
@@ -10,8 +10,8 @@ sheet_url <- "https://docs.google.com/spreadsheets/d/1HsBaNYXv4QR8DlIDqkMJaUx4fB
 df <- read_sheet(sheet_url, sheet = 2)
 
 df <- df %>%
-  mutate(Entity = "World") %>%
-  rename(Total_investment_billions_US_dollars = `Total Investment (in billions of U.S. Dollars)`) %>%
+  rename(total_private_investment_by_country = `Total Investment (in billions of U.S. Dollars)`) %>%
+  mutate(Entity = "World", total_private_investment_by_country = 10^9 * total_private_investment_by_country) %>%
   relocate(Entity, Year)
 
 # Private investment by geographic area Fig 4.2.4
@@ -19,19 +19,53 @@ df2 <- read_sheet(sheet_url, sheet = 4)
 
 df2 <- df2 %>%
   rename(Entity = `Geographic Area`,
-         Total_investment_billions_US_dollars = `Total Investment (in billions of U.S. Dollars)`)
+         total_private_investment_by_country = `Total Investment (in billions of U.S. Dollars)`) %>%
+  mutate(total_private_investment_by_country = 10^9 * total_private_investment_by_country, Year = 2021) %>%
+  relocate(Entity, Year)
 
 # Private investment by focus area Fig 4.2.10
 df3 <- read_sheet(sheet_url, sheet = 12)
 
 df3 <-df3 %>% 
-  mutate(Entity = "World") %>%
-  rename(Total_investment_billions_US_dollars = `Total Investment (in billions of U.S. Dollars)`,
-         Focus_area = `Focus Area`)
+  rename(Entity = "Focus Area", total_private_investment_by_focus_area = `Total Investment (in billions of U.S. Dollars)`) %>%
+  mutate(total_private_investment_by_focus_area = 10^9 * total_private_investment_by_focus_area) %>%
+  relocate(Entity, Year)
+
+df3 <- df3 %>%  
+  group_by(Year) %>% 
+  summarise(total_private_investment_by_focus_area = sum(total_private_investment_by_focus_area)) %>% 
+  bind_rows(df3, .)
+
+df3$Entity[is.na(df3$Entity)] <- "Total"
+
+# Change entity names
+df3 <- df3 %>% 
+  mutate(Entity=recode(Entity, 'AR/VR' = 'Augmented Reality/Virtual Reality', 
+                       'AV' = 'Audiovisual',
+                       'Agritech' = 'Agricultural Tech',
+                       'Ed Tech' = 'Educational Tech',
+                       'Fintech' = 'Financial Tech',
+                       'HR Tech' = 'Human Resources Tech',
+                       'Insurtech' = 'Insurance Tech',
+                       'NLP, Customer Support' = 'Natural Language, Customer Support',
+                       'VC' = 'Venture Capital'))
+
+# Private investment by geographic area Fig 4.2.6
+df4 <- read_sheet(sheet_url, sheet = 'Private AI Investment by US/China/EU (2013-21)')
+df4 <- df4 %>%
+  rename(Entity = `Geographic Area`,
+         total_private_investment_by_country = `Total Investment (in billions of U.S. Dollars)`) %>%
+  mutate(total_private_investment_by_country = 10^9 * total_private_investment_by_country) %>%
+  relocate(Entity, Year)
 
 # Merge and save
-df4 <- Reduce(function(x, y) merge(x, y, all=TRUE), 
-                 list(df, df2, df3))
-df4 <- df4 %>% relocate(Entity, Year)
+df5 <- Reduce(function(x, y) merge(x, y, all=TRUE), 
+              list(df, df2, df3, df4))
+df5 <- df5 %>% relocate(Entity, Year)
 
-write_csv(df4, "transformed/Private_investment_ai.csv")
+# Take care of inconsistency in numbers for US 2021 (in df2: 52878619257 vs df4: 52872119257)
+(52878619257+52872119257)/2
+df5[165, 3] <- 52875369257
+df5 <- df5[-166, ]
+
+write_csv(df5, "transformed/Private_investment_ai.csv")
