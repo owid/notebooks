5cb3c08d5742e850f71d955e0f91710d69f17bc4
Julia Broden <julia-broden@t-online.de>
2022-11-29 12:33:17 +0100

    Update master_script.R

--- JuliaBroden/code/master_script.R
+++ JuliaBroden/code/master_script.R
@@ -4,13 +4,13 @@ library(dplyr)
 library(readr)
 library(purrr)
 
-join <- list.files(path = "/Users/Julia/Documents/OWID/R/transformed",
+join <- list.files(path = "/Users/Julia/Documents/OWID/Clone/JuliaBroden/transformed",
                    pattern = "*.csv", full.names = T) %>% 
   lapply(read_csv) %>% 
   reduce(full_join, by = c('Entity', 'Year')) %>% 
   rename(entity = Entity, year = Year)
 
-path <- "/Users/Julia/Documents/OWID/R"
-write_csv(join, file.path(path, 'output/AI_index_22_11_12.csv'), na = "")
+path <- "/Users/Julia/Documents/OWID/Clone/JuliaBroden"
+write_csv(join, file.path(path, 'output/AI_index_22_11_29.csv'), na = "") 
 
-#setwd("~/Documents/OWID/R")
+# for the variables total_corporate_investment and total_corporate_investment_inflation_adjusted we get a rounding error in the staging server
