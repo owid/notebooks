6f52285aa3d15b1e9f4472161fef31b9f7e1b10d
Edouard Mathieu <edmat@pm.me>
2023-03-01 10:33:43 +0100

    Update script.R

--- EdouardMathieu/2021_12_bsl4_facilities_world_map/script.R
+++ EdouardMathieu/2021_12_bsl4_facilities_world_map/script.R
@@ -3,15 +3,16 @@ library(rvest)
 library(lubridate)
 library(plyr)
 rm(list = ls())
-setwd("~/git/notebooks/EdouardMathieu/2021_12_bsl4_facilities_world_map/")
 
 page <- read_html("https://en.wikipedia.org/wiki/Biosafety_level#Biosafety_level_4")
 tbl <- page %>% html_nodes("table") %>% html_table()
 
-df <- tbl[[4]]
+df <- tbl[[3]]
 setDT(df)
 df <- df[, .(year = year(today()), bsl4_facilities = .N), Country]
 setnames(df, "Country", "entity")
+
 df[, entity := mapvalues(entity, "Czech Republic", "Czechia")]
+df[, entity := mapvalues(entity, "Republic of China (Taiwan)", "Taiwan")]
 
 fwrite(df, "Biosafety level 4 facilities.csv")
