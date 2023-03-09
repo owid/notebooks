127c10f04ed39574f9a3ae8c47ecaa0a29163f34
Pablo Arriagada <paarriagadap@gmail.com>
2023-03-09 17:51:23 +0000

    refactor(wid): Update WID data

--- BetterDataDocs/PabloArriagada/WID/main.py
+++ BetterDataDocs/PabloArriagada/WID/main.py
@@ -8,21 +8,27 @@
 # - **Post-tax national income distribution** `diinc`, which includes both in-kind and in-cash redistribution.
 # - **Post-tax disposable income distribution** `cainc`, which excludes in-kind transfers (because the distribution of in-kind transfers requires a lot of assumptions).
 #
-# These distributions are the main DINA (distributional national accounts) income variables available at WID. DINA income concepts are distributed income concepts that are consistent with national accounts aggregates. The precise definitions are outlined in the [DINA guidelines](https://wid.world/es/news-article/2020-distributional-national-accounts-guidelines-dina-4/) and country-specific papers. 
+# These distributions are the main DINA (distributional national accounts) income variables available at WID. DINA income concepts are distributed income concepts that are consistent with national accounts aggregates. The precise definitions are outlined in the [DINA guidelines](https://wid.world/es/news-article/2020-distributional-national-accounts-guidelines-dina-4/) and country-specific papers.
 #
 # All of these distributions are generated using equal-split adults (j) as the population unit, meaning that the unit is the individual, but that income or wealth is distributed equally among all household members. The age group is individuals over age 20 (992, adult population), which excludes children (with 0 income in most of the cases). Extrapolations and interpolations are excluded from these files, as WID discourages its use at the level of individual countries (see the `exclude` description at `help wid` in Stata). More information about the variables and definitions can be found on [WID's codes dictionary](https://wid.world/codes-dictionary/).
 #
 # The variables processed in this notebook come from commands given in the `wid` function in Stata. These commands are located in the `wid_indices.do` file from this same folder. Selecting 'yes' in the code will execute this code and generate the most recent data from WID. Both `.csv` and `.dta` files are available for analysis in `data/raw/`.
 
-from create_dataset_functions import *
 import time
+from pathlib import Path
+
+from create_dataset_functions import *
+
+CURRENT_DIR = Path(__file__).parent.absolute()
 
 # ## Question: Do you want to run the WID query in Stata? (~10 minutes)
 # Choosing `yes` will run the Stata code first to get the most recent data. Choosing `no` will process data which was previously extracted from the Stata's do file.
 
 # +
-print('WARNING: If a full update is needed, first you have to run a Stata\'s query in a do file.')
-print('It usually takes about 10 minutes to execute.')
+print(
+    "WARNING: If a full update is needed, first you have to run a Stata's query in a do file."
+)
+print("It usually takes about 10 minutes to execute.")
 
 question = "Do you want to run this do file?"
 answer = query_yes_no(question)
@@ -39,19 +45,19 @@ run_stata(dofile, stata_dir, answer)
 # ## Load Stata's output and standardize
 # This step takes Stata's output, changes entities names to OWID standard, renames `year` to `Year` and multiplies shares variables by 100
 
-file = 'data/raw/wid_indices_992j.csv'
+file = CURRENT_DIR / "data/raw/wid_indices_992j.csv"
 df_final = load_and_standardize(file)
 
 # ## Reformat and export dataset
 # Variables are renamed for more human-readable columns to be uploaded in Grapher as a dataset.
 
-add_metadata_and_export(df_final, 'wid_pretax')
-add_metadata_and_export(df_final, 'wid_posttax_dis')
-add_metadata_and_export(df_final, 'wid_posttax_nat')
+add_metadata_and_export(df_final, "wid_pretax")
+add_metadata_and_export(df_final, "wid_posttax_dis")
+add_metadata_and_export(df_final, "wid_posttax_nat")
 
 end_time = time.time()
 elapsed_time = end_time - start_time
-print(f'The files were created in {elapsed_time} seconds :)')
-print('Update the pretax WID dataset with data/final/wid_pretax.csv')
-print('Update the post-tax disposable WID dataset with data/final/wid_posttax_dis.csv')
-print('Update the post-tax national WID dataset with data/final/wid_posttax_nat.csv')
+print(f"The files were created in {elapsed_time} seconds :)")
+print("Update the pretax WID dataset with data/final/wid_pretax.csv")
+print("Update the post-tax disposable WID dataset with data/final/wid_posttax_dis.csv")
+print("Update the post-tax national WID dataset with data/final/wid_posttax_nat.csv")
