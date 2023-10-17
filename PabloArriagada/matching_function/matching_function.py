529863c5bccadd462cf0b546cbbf2f7c9e7fd289
Pablo Arriagada <paarriagadap@gmail.com>
2023-10-17 18:08:22 +0100

    fix: pip

--- PabloArriagada/matching_function/matching_function.py
+++ PabloArriagada/matching_function/matching_function.py
@@ -15,24 +15,39 @@ PARENT_DIR = Path(__file__).parent.absolute()
 df = pd.read_feather(f"{PARENT_DIR}/tb.feather")
 df_percentiles = pd.read_feather(f"{PARENT_DIR}/tb_percentiles.feather")
 
+##############################################
 # FUNCTION SETTINGS
 
 series_type = "series_code"  # series_name or series_code
 
 # `series` depends on series_type: if series_type is series_name, series is a list of series names, e.g. ["WID Gini", "PIP Top 1% share"]; if series_type is series_code, series is a list of series codes, e.g. ["gini_wid_pretaxNational_perAdult", "p99p100Share_pip_disposable_perCapita"]
-series = ["gini_wid_pretaxNational_perAdult", "gini_lis_market_perCapita"]
+series = [
+    "gini_wid_pretaxNational_perAdult",
+    "gini_lis_market_perCapita",
+    "gini_pip_disposable_perCapita",
+]
 
 # reference_years is the list of years to match the series to. Each year is a dictionary with the year as key and a dictionary of parameters as value. The parameters are:
 # - maximum_distance: the maximum distance between the series and the reference year
 # - tie_break_strategy: the strategy to use to break ties when there are multiple series that match the reference year. The options are "lower" (select the series with the lowest distance to the reference year) or "higher" (select the series with the highest distance to the reference year)
 # - min_interval: the minimum distance between reference years. The value of min_interval for the last reference year is ignored.
 reference_years = {
-    1980: {"maximum_distance": 2, "tie_break_strategy": "lower", "min_interval": 10},
+    # 1980: {"maximum_distance": 0, "tie_break_strategy": "lower", "min_interval": 5},
     1990: {"maximum_distance": 3, "tie_break_strategy": "higher", "min_interval": 7},
     2000: {"maximum_distance": 0, "tie_break_strategy": "higher", "min_interval": 8},
     2010: {"maximum_distance": 3, "tie_break_strategy": "lower", "min_interval": 5},
     2018: {"maximum_distance": 5, "tie_break_strategy": "higher", "min_interval": 2},
 }
+
+# In case of PIP series, we need to define three parameters:
+# - constant_reporting_level: if True (no quotes), the series will be matched only to series with pipreportinglevel = "national". If False, series with pipreportinglevel = "urban" or "rural" will also be matched.
+# - constant_welfare_type: if True (no quotes), the series will be matched only to series with the pipwelfare defined in the income_or_consumpion parameter. If False, income or consumption can be selected.
+# - income_or_consumption: if constant_welfare_type is False, this parameter defines whether to match the series to income or consumption data. The options are "income" or "consumption".
+constant_reporting_level = True
+constant_welfare_type = False
+income_or_consumption = "income"
+
+##############################################
 
 
 def match_ref_years(
@@ -40,11 +55,16 @@ def match_ref_years(
     series_type: str,
     series: list,
     reference_years: dict,
-    # pip_strategy={"constant_reporting_level": True, "constant_welfare_type": False},
+    pip_strategy=dict,
 ) -> pd.DataFrame:
     """
     Match series to reference years.
     """
+
+    # Assert if the series belong to the df
+    assert set(series).issubset(set(df[series_type])), log.error(
+        f"The series {set(series) - set(df[series_type])} is not in the dataset."
+    )
     df_match = pd.DataFrame()
     df_series = df[df[series_type].isin(series)].reset_index(drop=True)
 
@@ -63,6 +83,31 @@ def match_ref_years(
         )
 
         df_year["distance"] = abs(df_year["year"] - y)
+
+        # If source is PIP, filter df_year according to constant_reporting_level and constant_welfare_type
+        # Filter df_year according to constant_reporting_level, constant_welfare_type and income_or_consumption
+
+        # Check if constant_reporting_level and constant_welfare_type are boolean
+        assert isinstance(constant_reporting_level, bool), log.error(
+            "`constant_reporting_level` must be boolean: True or False (without quotes)."
+        )
+        assert isinstance(constant_welfare_type, bool), log.error(
+            "`constant_welfare_type` must be boolean: True or False (without quotes)."
+        )
+        # Check if income_or_consumption is income or consumption
+        assert income_or_consumption in ["income", "consumption"], log.error(
+            "`income_or_consumption` must be either 'income' or 'consumption'."
+        )
+        if constant_reporting_level:
+            df_year = df_year[
+                (df_year["pipreportinglevel"] == "national")
+                | (df_year["pipreportinglevel"].isnull())
+            ].reset_index(drop=True)
+        if constant_welfare_type:
+            df_year = df_year[
+                (df_year["pipwelfare"] == income_or_consumption)
+                | (df_year["pipwelfare"].isnull())
+            ].reset_index(drop=True)
 
         # Merge the different reference years into a single dataframe
         if df_match.empty:
