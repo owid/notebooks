49393bc3c193b50a02f5f4c8ed222813e4fe0c21
Edouard Mathieu <edmat@pm.me>
2023-12-06 18:50:57 +0100

    update find-reusable-blocks

--- EdouardMathieu/find-reusable-blocks/find-reusable-blocks.py
+++ EdouardMathieu/find-reusable-blocks/find-reusable-blocks.py
@@ -1,7 +1,16 @@
+import argparse
 import pandas as pd
 
-# Change this to the name of the topic page
-PAGE = "Smoking"
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--slug", help="Name of the topic page")
+args = parser.parse_args()
+
+# Set the SLUG based on the command-line argument
+# If missing, raise an error
+if args.slug is None:
+    raise ValueError("Please provide a topic page name")
+SLUG = args.slug
 
 # URL to fetch data from
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR1Tylsvt9CQ5v5RsxXG0FuL_DdnMAdUWG7cSx6yY1aiVddg3w9YYQXdTCRxx62k7gg3VBYoMeShHHB/pub?gid=598437417&single=true&output=csv"
@@ -13,14 +22,14 @@ df = pd.read_csv(url)
 df = df[df["post_status"] == "publish"]
 
 # Get the rows for the specified topic page
-topic_page = df[df["post_title"] == PAGE]
+topic_page = df[df["post_slug"] == SLUG]
 
-print("Blocks used in the", PAGE, "topic page:\n")
+print("Blocks used in the", SLUG, "topic page:\n")
 print(topic_page[["reusable_block_slug", "reusable_block_title"]])
 
 blocks_used = topic_page["reusable_block_slug"].tolist()
 other_posts = df[
-    df["reusable_block_slug"].isin(blocks_used) & (df["post_title"] != PAGE)
+    df["reusable_block_slug"].isin(blocks_used) & (df["post_slug"] != SLUG)
 ]
 
 print("\nThese blocks are also used in:\n")
