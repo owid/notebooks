d668ae78f9e0193c308e3f9234f38145e68466ab
Pablo Arriagada <paarriagadap@gmail.com>
2024-06-19 15:55:03 +0100

    :lipstick: format code

--- PabloArriagada/global_distribution_giving_what_we_can/calculation.py
+++ PabloArriagada/global_distribution_giving_what_we_can/calculation.py
@@ -1,9 +1,21 @@
 """
 SCRIPT TO CALCULATE GLOBAL DISTRIBUTION OF GIVING WHAT WE CAN
-This script calculates the global distribution of Giving What We Can, using the World Bank PIP data, together with CPI and PPP conversion factors from the World Development Indicators. The script exports the results to a csv file.
+This script calculates the global distribution of Giving What We Can, using the World Bank PIP data, together with CPI and PPP conversion factors from the World Development Indicators.
+The script exports the results to a csv file.
 
-https://api.worldbank.org/v2/en/indicator/PA.NUS.PRVT.PP?downloadformat=csv
-https://api.worldbank.org/v2/en/indicator/FP.CPI.TOTL?downloadformat=csv
+The script requires the following files:
+    - A global distribution of income, created by Our World in Data using the World Bank PIP API.
+    - PPP conversion factors from the World Development Indicators.
+    - CPI data from the World Development Indicators.
+
+Follow these steps to run the script:
+    1. Update PIP_VERSION with the latest version of the PIP data, available at Our World in Data (it's the version of the garden step).
+    2. Download the PPP conversion factors from https://api.worldbank.org/v2/en/indicator/PA.NUS.PRVT.PP?downloadformat=csv
+    3. Download the CPI data from https://api.worldbank.org/v2/en/indicator/FP.CPI.TOTL?downloadformat=csv
+    4. Extract the data files to the same directory as this script.
+    5. Modify the file paths (PPP_FILE_PATH and CPI_FILE_PATH) if necessary, to coincide with the names of these new files.
+    6. Update MAX_YEAR_CPI with the latest year for which CPI data is available, if necessary.
+    7. Run this script.
 """
 
 from pathlib import Path
@@ -25,108 +37,132 @@ CPI_FILE_PATH = f"{PARENT_DIR}/API_FP.CPI.TOTL_DS2_en_csv_v2_381228.csv"
 # Define maximum year for CPI
 MAX_YEAR_CPI = 2022
 
-df_percentiles = pd.read_feather(PIP_URL)
 
-# Filter data to only include country=World and ppp_version = 2017
-df_percentiles = df_percentiles[(df_percentiles["country"] == "World")].reset_index(
-    drop=True
-)
+def run() -> None:
+    # Load data
+    df_percentiles = pd.read_feather(PIP_URL)
+    df_ppp_factors = pd.read_csv(PPP_FILE_PATH, skiprows=4)
+    df_cpi = pd.read_csv(CPI_FILE_PATH, skiprows=4)
 
-# Compare thr values with the one in the previous row
-df_percentiles["thr_previous"] = (
-    df_percentiles["thr"].groupby(df_percentiles["year"]).shift(1)
-)
-df_percentiles["thr_check"] = df_percentiles["thr"] >= df_percentiles["thr_previous"]
+    process_percentiles_and_export(df_percentiles)
+    combine_ppp_cpi_and_export(df_ppp_factors, df_cpi)
 
-# Assert that all values are True
-df_percentiles_check = df_percentiles[
-    (df_percentiles["thr_check"]) & (df_percentiles["percentile"] != 1)
-]
-assert df_percentiles_check[
-    "thr_check"
-].all(), "The global distribution is not monotonically increasing"
 
-# Keep only the relevant columns
-df_percentiles = df_percentiles[["country", "year", "percentile", "thr"]]
+def process_percentiles_and_export(df_percentiles: pd.DataFrame) -> None:
+    """
+    Process the percentiles dataframe to calculate the global distribution of Giving What We Can.
+    """
+    # Filter data to only include country=World and ppp_version = 2017
+    df_percentiles = df_percentiles[(df_percentiles["country"] == "World")].reset_index(
+        drop=True
+    )
 
-# Filter for the latest year
-df_percentiles = df_percentiles[
-    df_percentiles["year"] == df_percentiles["year"].max()
-].reset_index(drop=True)
+    # Compare thr values with the one in the previous row
+    df_percentiles["thr_previous"] = (
+        df_percentiles["thr"].groupby(df_percentiles["year"]).shift(1)
+    )
+    df_percentiles["thr_check"] = (
+        df_percentiles["thr"] >= df_percentiles["thr_previous"]
+    )
 
-# Export to csv
-df_percentiles.to_csv(f"{PARENT_DIR}/pip_global_percentiles.csv", index=False)
+    # Assert that all values are True
+    df_percentiles_check = df_percentiles[
+        (df_percentiles["thr_check"]) & (df_percentiles["percentile"] != 1)
+    ]
+    assert df_percentiles_check[
+        "thr_check"
+    ].all(), "The global distribution is not monotonically increasing"
 
-# PPP CONVERSION FACTORS
-# Load ppp conversion factors and CPI
-df_ppp_factors = pd.read_csv(PPP_FILE_PATH, skiprows=4)
-df_cpi = pd.read_csv(CPI_FILE_PATH, skiprows=4)
+    # Keep only the relevant columns
+    df_percentiles = df_percentiles[["country", "year", "percentile", "thr"]]
 
-# Remove columns containing "Unnamed" in their name
-columns_to_drop = [col for col in df_ppp_factors.columns if "Unnamed" in col]
-df_ppp_factors = df_ppp_factors.drop(columns=columns_to_drop)
+    # Filter for the latest year
+    df_percentiles = df_percentiles[
+        df_percentiles["year"] == df_percentiles["year"].max()
+    ].reset_index(drop=True)
 
-# Remove columns containing "Unnamed" in their name
-columns_to_drop = [col for col in df_cpi.columns if "Unnamed" in col]
-df_cpi = df_cpi.drop(columns=columns_to_drop)
+    # Export to csv
+    df_percentiles.to_csv(f"{PARENT_DIR}/pip_global_percentiles.csv", index=False)
 
-# Drop "Indicator Name","Indicator Code"
-columns_to_drop = ["Indicator Name", "Indicator Code"]
-df_ppp_factors = df_ppp_factors.drop(columns=columns_to_drop)
-df_cpi = df_cpi.drop(columns=columns_to_drop)
 
-# Select in df_ppp_factors only the columns of interest (Country Name, Country Code, 2017)
-df_ppp_factors = df_ppp_factors[["Country Name", "Country Code", "2017"]]
-df_ppp_factors = df_ppp_factors.rename(
-    columns={
-        "Country Name": "country",
-        "Country Code": "country_code",
-        "2017": "ppp_2017",
-    }
-)
+def combine_ppp_cpi_and_export(
+    df_ppp_factors: pd.DataFrame, df_cpi: pd.DataFrame
+) -> None:
+    """
+    Combine the PPP and CPI dataframes and export the result.
+    """
+    # Remove columns containing "Unnamed" in their name
+    columns_to_drop = [col for col in df_ppp_factors.columns if "Unnamed" in col]
+    df_ppp_factors = df_ppp_factors.drop(columns=columns_to_drop)
 
-# Make df_cpi long
-df_cpi = df_cpi.melt(
-    id_vars=["Country Name", "Country Code"], var_name="year", value_name="cpi"
-)
-df_cpi = df_cpi.rename(
-    columns={"Country Name": "country", "Country Code": "country_code"}
-)
+    # Remove columns containing "Unnamed" in their name
+    columns_to_drop = [col for col in df_cpi.columns if "Unnamed" in col]
+    df_cpi = df_cpi.drop(columns=columns_to_drop)
 
-# Make year integer
-df_cpi["year"] = df_cpi["year"].astype(int)
+    # Drop "Indicator Name","Indicator Code"
+    columns_to_drop = ["Indicator Name", "Indicator Code"]
+    df_ppp_factors = df_ppp_factors.drop(columns=columns_to_drop)
+    df_cpi = df_cpi.drop(columns=columns_to_drop)
 
-# Filter for 2017 and the maximum year
-df_cpi = df_cpi[
-    (df_cpi["year"] == 2017) | (df_cpi["year"] == MAX_YEAR_CPI)
-].reset_index(drop=True)
+    # Select in df_ppp_factors only the columns of interest (Country Name, Country Code, 2017)
+    df_ppp_factors = df_ppp_factors[["Country Name", "Country Code", "2017"]]
+    df_ppp_factors = df_ppp_factors.rename(
+        columns={
+            "Country Name": "country",
+            "Country Code": "country_code",
+            "2017": "ppp_2017",
+        }
+    )
 
-# Make table wide, by naming the columns cpi_2017 and cpi_max
-df_cpi = df_cpi.pivot(index="country_code", columns="year", values="cpi").reset_index()
+    # Make df_cpi long
+    df_cpi = df_cpi.melt(
+        id_vars=["Country Name", "Country Code"], var_name="year", value_name="cpi"
+    )
+    df_cpi = df_cpi.rename(
+        columns={"Country Name": "country", "Country Code": "country_code"}
+    )
 
-# Rename columns
-df_cpi = df_cpi.rename(columns={2017: "cpi_2017", MAX_YEAR_CPI: f"cpi_{MAX_YEAR_CPI}"})
+    # Make year integer
+    df_cpi["year"] = df_cpi["year"].astype(int)
 
-print(df_cpi)
+    # Filter for 2017 and the maximum year
+    df_cpi = df_cpi[
+        (df_cpi["year"] == 2017) | (df_cpi["year"] == MAX_YEAR_CPI)
+    ].reset_index(drop=True)
 
-# Calculate the inflation rate between 2017 and the maximum year
-df_cpi["inflation_rate"] = df_cpi[f"cpi_{MAX_YEAR_CPI}"] / df_cpi["cpi_2017"]
+    # Make table wide, by naming the columns cpi_2017 and cpi_max
+    df_cpi = df_cpi.pivot(
+        index="country_code", columns="year", values="cpi"
+    ).reset_index()
 
-# Merge df_ppp_factors and df_cpi
-df_ppp = df_ppp_factors.merge(
-    df_cpi[["country_code", "cpi_2017", f"cpi_{MAX_YEAR_CPI}", "inflation_rate"]],
-    on="country_code",
-    how="left",
-)
+    # Rename columns
+    df_cpi = df_cpi.rename(
+        columns={2017: "cpi_2017", MAX_YEAR_CPI: f"cpi_{MAX_YEAR_CPI}"}
+    )
 
-# Calculate the ppp conversion factor for the most recent year
-df_ppp["ppp_factor"] = df_ppp["ppp_2017"] * df_ppp["inflation_rate"]
+    # Calculate the inflation rate between 2017 and the maximum year
+    df_cpi["inflation_rate"] = df_cpi[f"cpi_{MAX_YEAR_CPI}"] / df_cpi["cpi_2017"]
 
-# Remove empty values of pa_nus_prvt_pp
-df_ppp = df_ppp[~df_ppp["ppp_factor"].isna()].reset_index(drop=True)
+    # Merge df_ppp_factors and df_cpi
+    df_ppp = pd.merge(
+        df_ppp_factors,
+        df_cpi[["country_code", "cpi_2017", f"cpi_{MAX_YEAR_CPI}", "inflation_rate"]],
+        on="country_code",
+        how="left",
+    )
 
-# Sort by country
-df_ppp = df_ppp.sort_values(by=["country"]).reset_index(drop=True)
+    # Calculate the ppp conversion factor for the most recent year
+    df_ppp["ppp_factor"] = df_ppp["ppp_2017"] * df_ppp["inflation_rate"]
 
-# Export to csv
-df_ppp.to_csv(f"{PARENT_DIR}/wdi_ppp.csv", index=False)
+    # Remove empty values of pa_nus_prvt_pp
+    df_ppp = df_ppp[~df_ppp["ppp_factor"].isna()].reset_index(drop=True)
+
+    # Sort by country
+    df_ppp = df_ppp.sort_values(by=["country"]).reset_index(drop=True)
+
+    # Export to csv
+    df_ppp.to_csv(f"{PARENT_DIR}/wdi_ppp.csv", index=False)
+
+
+if __name__ == "__main__":
+    run()
