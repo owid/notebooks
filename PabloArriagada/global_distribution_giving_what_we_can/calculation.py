92d3ae05074f23312dda51f2162a2eef61a28502
Pablo Arriagada <paarriagadap@gmail.com>
2024-06-19 10:36:26 +0100

    :sparkles: format code

--- PabloArriagada/global_distribution_giving_what_we_can/calculation.py
+++ PabloArriagada/global_distribution_giving_what_we_can/calculation.py
@@ -13,7 +13,7 @@ import pandas as pd
 PARENT_DIR = Path(__file__).parent.absolute()
 
 # Define OWID version of PIP data
-PIP_VERSION = '2024-03-27'
+PIP_VERSION = "2024-03-27"
 
 # Define PIP percentiles URL
 PIP_URL = f"http://catalog.ourworldindata.org/garden/wb/{PIP_VERSION}/world_bank_pip/percentiles_income_consumption_2017.feather"
@@ -28,24 +28,34 @@ MAX_YEAR_CPI = 2022
 df_percentiles = pd.read_feather(PIP_URL)
 
 # Filter data to only include country=World and ppp_version = 2017
-df_percentiles = df_percentiles[(df_percentiles['country'] == 'World')].reset_index(drop=True)
+df_percentiles = df_percentiles[(df_percentiles["country"] == "World")].reset_index(
+    drop=True
+)
 
 # Compare thr values with the one in the previous row
-df_percentiles['thr_previous'] = df_percentiles['thr'].groupby(df_percentiles['year']).shift(1)
-df_percentiles['thr_check'] = df_percentiles['thr'] >= df_percentiles['thr_previous']
+df_percentiles["thr_previous"] = (
+    df_percentiles["thr"].groupby(df_percentiles["year"]).shift(1)
+)
+df_percentiles["thr_check"] = df_percentiles["thr"] >= df_percentiles["thr_previous"]
 
 # Assert that all values are True
-df_percentiles_check = df_percentiles[(df_percentiles['thr_check']) & (df_percentiles['percentile'] != 1)]
-assert df_percentiles_check['thr_check'].all(), 'The global distribution is not monotonically increasing'
+df_percentiles_check = df_percentiles[
+    (df_percentiles["thr_check"]) & (df_percentiles["percentile"] != 1)
+]
+assert df_percentiles_check[
+    "thr_check"
+].all(), "The global distribution is not monotonically increasing"
 
 # Keep only the relevant columns
-df_percentiles = df_percentiles[['country', 'year', 'percentile', "thr"]]
+df_percentiles = df_percentiles[["country", "year", "percentile", "thr"]]
 
 # Filter for the latest year
-df_percentiles = df_percentiles[df_percentiles['year'] == df_percentiles['year'].max()].reset_index(drop=True)
+df_percentiles = df_percentiles[
+    df_percentiles["year"] == df_percentiles["year"].max()
+].reset_index(drop=True)
 
 # Export to csv
-df_percentiles.to_csv(f'{PARENT_DIR}/pip_global_percentiles.csv', index=False)
+df_percentiles.to_csv(f"{PARENT_DIR}/pip_global_percentiles.csv", index=False)
 
 # PPP CONVERSION FACTORS
 # Load ppp conversion factors and CPI
@@ -53,57 +63,70 @@ df_ppp_factors = pd.read_csv(PPP_FILE_PATH, skiprows=4)
 df_cpi = pd.read_csv(CPI_FILE_PATH, skiprows=4)
 
 # Remove columns containing "Unnamed" in their name
-columns_to_drop = [col for col in df_ppp_factors.columns if 'Unnamed' in col]
+columns_to_drop = [col for col in df_ppp_factors.columns if "Unnamed" in col]
 df_ppp_factors = df_ppp_factors.drop(columns=columns_to_drop)
 
 # Remove columns containing "Unnamed" in their name
-columns_to_drop = [col for col in df_cpi.columns if 'Unnamed' in col]
+columns_to_drop = [col for col in df_cpi.columns if "Unnamed" in col]
 df_cpi = df_cpi.drop(columns=columns_to_drop)
 
 # Drop "Indicator Name","Indicator Code"
-columns_to_drop = ['Indicator Name', 'Indicator Code']
+columns_to_drop = ["Indicator Name", "Indicator Code"]
 df_ppp_factors = df_ppp_factors.drop(columns=columns_to_drop)
 df_cpi = df_cpi.drop(columns=columns_to_drop)
 
 # Select in df_ppp_factors only the columns of interest (Country Name, Country Code, 2017)
-df_ppp_factors = df_ppp_factors[['Country Name', 'Country Code', '2017']]
-df_ppp_factors = df_ppp_factors.rename(columns={'Country Name': 'country', 'Country Code': 'country_code', '2017': 'ppp_2017'})
+df_ppp_factors = df_ppp_factors[["Country Name", "Country Code", "2017"]]
+df_ppp_factors = df_ppp_factors.rename(
+    columns={
+        "Country Name": "country",
+        "Country Code": "country_code",
+        "2017": "ppp_2017",
+    }
+)
 
 # Make df_cpi long
-df_cpi = df_cpi.melt(id_vars=['Country Name', 'Country Code'], var_name='year', value_name='cpi')
-df_cpi = df_cpi.rename(columns={'Country Name': 'country', 'Country Code': 'country_code'})
+df_cpi = df_cpi.melt(
+    id_vars=["Country Name", "Country Code"], var_name="year", value_name="cpi"
+)
+df_cpi = df_cpi.rename(
+    columns={"Country Name": "country", "Country Code": "country_code"}
+)
 
 # Make year integer
-df_cpi['year'] = df_cpi['year'].astype(int)
+df_cpi["year"] = df_cpi["year"].astype(int)
 
 # Filter for 2017 and the maximum year
-df_cpi = df_cpi[(df_cpi['year'] == 2017) | (df_cpi['year'] == MAX_YEAR_CPI)].reset_index(drop=True)
+df_cpi = df_cpi[
+    (df_cpi["year"] == 2017) | (df_cpi["year"] == MAX_YEAR_CPI)
+].reset_index(drop=True)
 
 # Make table wide, by naming the columns cpi_2017 and cpi_max
-df_cpi = df_cpi.pivot(index='country_code', columns='year', values='cpi').reset_index()
+df_cpi = df_cpi.pivot(index="country_code", columns="year", values="cpi").reset_index()
 
 # Rename columns
-df_cpi = df_cpi.rename(columns={2017: 'cpi_2017', MAX_YEAR_CPI: f'cpi_{MAX_YEAR_CPI}'})
+df_cpi = df_cpi.rename(columns={2017: "cpi_2017", MAX_YEAR_CPI: f"cpi_{MAX_YEAR_CPI}"})
 
 print(df_cpi)
 
 # Calculate the inflation rate between 2017 and the maximum year
-df_cpi['inflation_rate'] = df_cpi[f'cpi_{MAX_YEAR_CPI}'] / df_cpi['cpi_2017']
+df_cpi["inflation_rate"] = df_cpi[f"cpi_{MAX_YEAR_CPI}"] / df_cpi["cpi_2017"]
 
 # Merge df_ppp_factors and df_cpi
-df_ppp = df_ppp_factors.merge(df_cpi[["country_code", "cpi_2017", f"cpi_{MAX_YEAR_CPI}", "inflation_rate"]], on='country_code', how='left')
+df_ppp = df_ppp_factors.merge(
+    df_cpi[["country_code", "cpi_2017", f"cpi_{MAX_YEAR_CPI}", "inflation_rate"]],
+    on="country_code",
+    how="left",
+)
 
 # Calculate the ppp conversion factor for the most recent year
-df_ppp['ppp_factor'] = df_ppp['ppp_2017'] * df_ppp['inflation_rate']
+df_ppp["ppp_factor"] = df_ppp["ppp_2017"] * df_ppp["inflation_rate"]
 
 # Remove empty values of pa_nus_prvt_pp
-df_ppp = df_ppp[~df_ppp['ppp_factor'].isna()].reset_index(drop=True)
+df_ppp = df_ppp[~df_ppp["ppp_factor"].isna()].reset_index(drop=True)
 
 # Sort by country
-df_ppp = df_ppp.sort_values(by=['country']).reset_index(drop=True)
+df_ppp = df_ppp.sort_values(by=["country"]).reset_index(drop=True)
 
 # Export to csv
-df_ppp.to_csv(f'{PARENT_DIR}/wdi_ppp.csv', index=False)
-
-
-
+df_ppp.to_csv(f"{PARENT_DIR}/wdi_ppp.csv", index=False)
