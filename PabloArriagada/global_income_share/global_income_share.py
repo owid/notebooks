e7bd999645835c485e17ea9615bbea876b446280
Pablo Arriagada <paarriagadap@gmail.com>
2025-03-11 16:59:17 +0000

    :sparkles: multiple years

--- PabloArriagada/global_income_share/global_income_share.py
+++ PabloArriagada/global_income_share/global_income_share.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 
 PARENT_DIR = Path(__file__).parent.absolute()
 
@@ -17,40 +15,35 @@ DATASET_URL = f"http://catalog.ourworldindata.org/garden/wb/{VERSION}/thousand_b
 # Read data
 df = pd.read_feather(DATASET_URL)
 
-# Select only for the maximum year
-df = df[df["year"] == df["year"].max()].reset_index(drop=True)
+# Sort data by year and avg
+df = df.sort_values(by=["year", "avg"], ascending=[True, True]).reset_index(drop=True)
 
-# Sort data by avg
-df = df.sort_values(by="avg", ascending=True).reset_index(drop=True)
+# Calculate the cumulative sum of the population by year
+df["cum_pop"] = df.groupby("year")["pop"].cumsum()
 
-# Calculate the cumulative sum of the population
-df["cum_pop"] = df["pop"].cumsum()
+# Calculate the global population as the last value of cum_pop by year
+df["global_population"] = df.groupby("year")["cum_pop"].transform("max")
 
-# Calculate the global population as the last value of cum_pop
-global_population = df["cum_pop"].iloc[-1]
+# Calculate the cumulative sum of the population as a percentage of the global population by year
+df["cum_pop_perc"] = df["cum_pop"] / df["global_population"] * 100
 
-# Calculate the cumulative sum of the population as a percentage of the global population
-df["cum_pop_perc"] = df["cum_pop"] / global_population * 100
-
-# Define the cum_pop_perc that is closest to 10, 20, 30, ..., 100 and assign it to all the other rows
+# Define the cum_pop_perc that is closest to 10, 20, 30, ..., 100 and assign it to all the other rows by year
 df["decile"] = df["cum_pop_perc"].apply(lambda x: np.ceil(x / 10))
 
-# Calculate the weighted average of avg, using pop as weights, by decile
+# Calculate the weighted average of avg, using pop as weights, by decile and year
 df["avg"] = df["avg"] * df["pop"]
-df = df.groupby("decile").agg(
+df = df.groupby(["year", "decile"]).agg(
     {
         "avg": "sum",
         "pop": "sum",
     }
 ).reset_index()
 
-# Divide avg by the total avg, to get the share of the total income
-df["share"] = df["avg"] / df["avg"].sum() * 100
+# Divide avg by the total avg, to get the share of the total income by year
+df["share"] = df.groupby("year")["avg"].transform(lambda x: x / x.sum() * 100)
 
-# Divide avg by the total population, to get the average income per person
+# Divide avg by the total population, to get the average income per person by year
 df["avg"] = df["avg"] / df["pop"]
-
-print(df)
 
 # Save to csv
 df.to_csv(PARENT_DIR / "global_income_distribution.csv", index=False)
