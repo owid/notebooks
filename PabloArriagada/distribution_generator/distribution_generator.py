fabd85eca73b3f54e6dbfc82f8a36995fa54ce1b
Pablo Arriagada <paarriagadap@gmail.com>
2025-03-14 16:37:32 +0000

    :construction: copy function

--- PabloArriagada/distribution_generator/distribution_generator.py
+++ PabloArriagada/distribution_generator/distribution_generator.py
@@ -52,9 +52,22 @@ def run() -> None:
             log_scale=True,
             multiple="layer",
             hue="country",
-            ridge_plot=False,
             hue_order=countries,
-            years=None,
+            years=[2024],
+            legend=True,
+            common_norm=False,
+        )
+
+        distributional_plots_per_row(
+            data=df_thousand_bins,
+            df_main_indicators=df_main_indicators,
+            x="avg",
+            weights="pop",
+            log_scale=True,
+            multiple="layer",
+            hue="country",
+            hue_order=["Ethiopia", "Bangladesh", "Vietnam", "Turkey", "United States"],
+            years=[2024],
             legend=True,
             common_norm=False,
         )
@@ -68,7 +81,6 @@ def run() -> None:
         log_scale=True,
         multiple="stack",
         hue="country",
-        ridge_plot=False,
         hue_order=["Denmark", "Ethiopia"],
         years=[2024],
         legend=True,
@@ -84,7 +96,142 @@ def distributional_plots(
     log_scale: bool,
     multiple: str,
     hue: str,
-    ridge_plot: bool = False,
+    hue_order: List[str] = None,
+    years: List[int] = None,
+    legend: bool = True,
+    common_norm: bool = True,
+) -> None:
+    """
+    Plot distributional data with seaborn, with multiple options for customization.
+    """
+
+    # If no years are provided, use all years in the data
+    if years is None:
+        years = list(data["year"].unique())
+
+    # Filter data by years
+    data = data[data["year"].isin(years)].reset_index(drop=True)
+
+    # Define filename according to the hue_order
+    if len(hue_order) <= 5:
+        filename = "_".join(hue_order)
+    elif len(hue_order) > 5:
+        filename = "multiple_countries"
+    else:
+        filename = "all_countries"
+
+    for year in years:
+        data_year = data[data["year"] == year].reset_index(drop=True)
+
+        # Define world mean
+        world_mean_year = df_main_indicators.loc[
+            (df_main_indicators["country"] == "World")
+            & (df_main_indicators["year"] == year),
+            "mean",
+        ].values[0]
+
+        # Define world median
+        world_median_year = df_main_indicators.loc[
+            (df_main_indicators["country"] == "World")
+            & (df_main_indicators["year"] == year),
+            "median",
+        ].values[0]
+
+        # Plot a kde with seaborn
+        kde_plot = sns.kdeplot(
+            data=data_year,
+            x=x,
+            weights=weights,
+            fill=True,
+            log_scale=log_scale,
+            hue=hue,
+            hue_order=hue_order,
+            multiple=multiple,
+            legend=legend,
+            common_norm=common_norm,
+        )
+
+        if log_scale:
+            # Customize x-axis ticks to show 1, 2, 5, 10, 20, 50, 100, etc.
+            # kde_plot.set(xscale="log")
+            kde_plot.set_xticks([1, 2, 5, 10, 20, 50, 100, 200, 500, 1000])
+            kde_plot.get_xaxis().set_major_formatter(plt.ScalarFormatter())
+
+        # Add a vertical line for the international poverty line
+        plt.axvline(
+            x=INTERNATIONAL_POVERTY_LINE,
+            color="lightgrey",
+            linestyle="--",
+            linewidth=0.8,
+        )
+        plt.text(
+            x=INTERNATIONAL_POVERTY_LINE,  # x-coordinate for the text
+            y=plt.ylim()[1]
+            * 0.99,  # y-coordinate for the text, positioned near the top of the plot
+            s=f"International Poverty Line: ${INTERNATIONAL_POVERTY_LINE}",  # Text string to display
+            color="grey",  # Color of the text
+            rotation=90,  # Rotate the text 90 degrees
+            verticalalignment="top",  # Align the text vertically at the top
+            fontsize=8,  # Font size of the text
+        )
+
+        # Add a vertical line for the world mean, in the same format as the international poverty line
+        plt.axvline(
+            x=world_mean_year,
+            color="lightgrey",
+            linestyle="--",
+            linewidth=0.8,
+        )
+        plt.text(
+            x=world_mean_year,
+            y=plt.ylim()[1] * 0.99,
+            s=f"World mean: ${round(world_mean_year,2):.2f}",
+            color="grey",
+            rotation=90,
+            verticalalignment="top",
+            fontsize=8,
+        )
+
+        # Add a vertical line for the world median, in the same format as the international poverty line
+        plt.axvline(
+            x=world_median_year,
+            color="lightgrey",
+            linestyle="--",
+            linewidth=0.8,
+        )
+        plt.text(
+            x=world_median_year,
+            y=plt.ylim()[1] * 0.99,
+            s=f"World median: ${round(world_median_year,2):.2f}",
+            color="grey",
+            rotation=90,
+            verticalalignment="top",
+            fontsize=8,
+        )
+
+        if legend:
+            # Move the legend inside the plot
+            kde_plot.legend_.set_bbox_to_anchor((0.8, 0.8))
+            kde_plot.legend_.set_loc("upper left")
+
+        fig = kde_plot.get_figure()
+        fig.set_size_inches(WIDTH / 100, HEIGHT / 100)
+        fig.savefig(
+            f"{PARENT_DIR}/{filename}_{year}_log_{log_scale}_multiple_{multiple}_common_norm_{common_norm}.svg"
+        )
+        plt.close(fig)
+
+    return None
+
+
+def distributional_plots_per_row(
+    data: pd.DataFrame,
+    df_main_indicators: pd.DataFrame,
+    x: str,
+    weights: str,
+    log_scale: bool,
+    multiple: str,
+    hue: str,
     hue_order: List[str] = None,
     years: List[int] = None,
     legend: bool = True,
